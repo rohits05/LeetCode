678a1baa736a7e8cfc8ab391eff4ed4120494c79
Rohit Shaw <carpediem212045@gmail.com>
2022-07-20 21:13:36 +0530

    Time: 1 ms (87.68%), Space: 42.1 MB (56.34%) - LeetHub

---
+++ 394-decode-string/394-decode-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeString(String s) {
+        Deque<Character> queue = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            queue.offer(c);
+        }
+        return helper(queue);
+    }
+    
+    public String helper(Deque<Character> queue) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        while (!queue.isEmpty()) {
+            char c= queue.poll();
+            if (Character.isDigit(c)) {
+                num = num * 10 + c - '0';
+            } else if (c == '[') {
+                String sub = helper(queue);
+                for (int i = 0; i < num; i++) {
+                    sb.append(sub);    
+                }
+                num = 0;
+            } else if (c == ']') {
+                break;
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
