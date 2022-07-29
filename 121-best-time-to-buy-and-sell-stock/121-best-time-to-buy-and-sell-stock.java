de986a7ced1aa815f8654d8d4cdb8b99aeb930c8
Rohit Shaw <carpediem212045@gmail.com>
2022-07-29 12:27:59 +0530

    Time: 3 ms (67.17%), Space: 76.5 MB (78.49%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -4,10 +4,12 @@ class Solution {
         int minI = Integer.MAX_VALUE;
         
         for(int i=0; i<prices.length; i++){
-            if(prices[i] < minI) 
-                minI = prices[i];
-            else if(prices[i] - minI > profit)
-                profit = prices[i] - minI;
+            // if(prices[i] < minI) 
+            //     minI = prices[i];
+            // else if(prices[i] - minI > profit)
+            //     profit = prices[i] - minI;
+            minI = Math.min(minI, prices[i]);
+            profit = Math.max(profit, prices[i] - minI);
         }
         return profit;
     }
