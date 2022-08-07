5a272c80365bf086ca9abbc6a21991a65f626902
Rohit Shaw <carpediem212045@gmail.com>
2022-08-07 20:21:14 +0530

    Time: 7 ms (85.05%), Space: 5.8 MB (95.33%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mod = 1e9+7; // 1_000_000_007
+    int countVowelPermutation(int n) {
+        long a = 1;
+        long e = 1;
+        long i = 1;
+        long o = 1;
+        long u = 1;
+        
+        for(int k=1; k<n; k++){
+            long aa = a;
+            long ee = e;
+            long ii = i;
+            long oo = o;
+            long uu = u;
+            a = (ee + ii + uu) % mod;
+            e = (aa + ii) % mod;
+            i = (ee + oo) % mod;
+            o = ii % mod;
+            u = (ii + oo) % mod;
+        }
+        
+        long res = ((((a + e) % mod + i) % mod + o) % mod + u) % mod; 
+        return (int) res;
+    } 
+};
