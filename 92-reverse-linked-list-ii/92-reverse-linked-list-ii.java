4459ed9608bc5482d7b106b0bec2b032bc547563
Rohit Shaw <carpediem212045@gmail.com>
2022-08-15 12:54:49 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (8.68%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -10,23 +10,51 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {  
-        if(head == null)  return null;
+//         if(head == null)  return null;
         
-        ListNode dummy = new ListNode(0); 
-        dummy.next = head;
-        ListNode pre = dummy;
+//         ListNode dummy = new ListNode(0); 
+//         dummy.next = head;
+//         ListNode pre = dummy;
         
-        for(int i=0; i<m-1; i++) pre = pre.next;
+//         for(int i=0; i<m-1; i++) pre = pre.next;
 
-        ListNode start = pre.next;
-        ListNode then = start.next; 
+//         ListNode start = pre.next;
+//         ListNode then = start.next; 
 
-        for(int i=0; i<n-m; i++) {
-            start.next = then.next;
-            then.next = pre.next;
-            pre.next = then;
-            then = start.next;
+//         for(int i=0; i<n-m; i++) {
+//             start.next = then.next;
+//             then.next = pre.next;
+//             pre.next = then;
+//             then = start.next;
+//         }
+//         return dummy.next;
+        
+        // Anotherc~ Approach
+        if(head == null) return null;
+        
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while(m > 1) {
+            prev = curr;
+            curr = curr.next;
+            m--; n--;
         }
-        return dummy.next;
+        
+        ListNode connect = prev;
+        ListNode tail = curr;
+        while(n > 0) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            n--;
+        }
+        
+        if(connect != null) connect.next = prev;
+        else head = prev;;
+        
+        tail.next = curr;
+        return head;
     }
 }
