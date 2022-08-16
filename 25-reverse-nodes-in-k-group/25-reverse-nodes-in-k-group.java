c515b3f85475a303f4427446f7790f74762ecaa0
Rohit Shaw <carpediem212045@gmail.com>
2022-08-16 12:39:54 +0530

    Time: 1 ms (63.26%), Space: 46.6 MB (5.47%) - LeetHub

---
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || k == 1) return head;
+        
+        ListNode dummy = new ListNode(0); // Creating Dummy Node
+        dummy.next = head; // Poinying it to the Initial head of LL
+        
+        ListNode cur = dummy, prev = dummy, nex = dummy;
+        int count = 0;
+        
+        // Calculating the entire length of LL
+        while(cur.next != null){
+            cur = cur.next;
+            count++;
+        }
+        
+        // Traverse till Required Grp Size
+        while(count >= k){
+            cur = prev.next; // 1st Node of the Grp
+            nex = cur.next; // 2nd Node of the Grp
+            
+            // Reversing K-1 links i.e of Size K Grps
+            for(int i=1; i<k; i++){ 
+                cur.next = nex.next;
+                nex.next = prev.next;
+                prev.next = nex;
+                nex = cur.next;
+            }
+            prev = cur; // At last node of prev reversed Grp
+            count -= k; // Current size of Grp
+        }
+        
+        return dummy.next; // Head of LL
+    }
+}
