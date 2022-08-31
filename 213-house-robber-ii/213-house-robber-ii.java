9ce22d6bfafde0913b7bc32be5129aff8adcb8e3
Rohit Shaw <carpediem212045@gmail.com>
2022-08-31 11:10:46 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (33.70%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        
+        if(n == 1) return nums[0];
+        return Math.max( ways(nums, 0, n-1), ways(nums,1, n));
+    }
+    private int ways(int[] nums, int start, int end){
+        int prev2 =0, prev = 0, curri =0;
+        
+        for(int i=start; i<end; i++){
+            curri = Math.max(prev2 + nums[i], prev);
+            prev2 = prev;
+            prev = curri;
+        }
+        return curri;
+    }
+}
