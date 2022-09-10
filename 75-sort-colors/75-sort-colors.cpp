5c8beda4f0cb6b053aa79fef249e0113fac09dff
Rohit Shaw <carpediem212045@gmail.com>
2022-09-10 09:55:57 +0530

    Time: 9 ms (6.68%), Space: 8.1 MB (90.70%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-    }
+        // sort(nums.begin(), nums.end());
+        
+        int temp =0, start =0, mid =0, end =nums.size() - 1;
+    
+        while(mid <= end) {
+            if(nums[mid] == 0) {
+                temp = nums[start];
+                nums[start] = nums[mid];
+                nums[mid] = temp;
+                start++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else if(nums[mid] == 2) {
+                temp = nums[end];
+                nums[end] = nums[mid];
+                nums[mid] = temp;
+                end--;
+            }
+        }
+     }
 };
