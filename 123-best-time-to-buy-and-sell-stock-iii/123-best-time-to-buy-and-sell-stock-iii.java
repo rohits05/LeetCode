2ee662de634522367f4256e68832adac32eb2b7c
Rohit Shaw <carpediem212045@gmail.com>
2022-09-10 15:28:13 +0530

    Time: 6 ms (75.52%), Space: 78.3 MB (69.10%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProfit(int[] price) {
        int n = price.length;
         
-       // Space Memoization Technique!!
+       // Space Optimization Technique!!
        int buy1 = Integer.MAX_VALUE;
        int buy2 = Integer.MAX_VALUE;
        int profit1 = 0;
