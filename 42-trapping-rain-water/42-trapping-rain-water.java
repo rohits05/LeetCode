1cd0424c68b04971900f2882111bb5df918560b9
Rohit Shaw <carpediem212045@gmail.com>
2022-09-18 18:13:09 +0530

    Time: 1 ms (99.76%), Space: 48.6 MB (62.44%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height == null || height.length == 0) return 0;
+        
+        int n = height.length - 1;
+        int level = 0, water = 0, index = 0;
+        
+        while(n > 0) {
+            int lower = height[index] < height[index + n] ? height[index++] : height[index + n];
+            
+			if(lower > level) level = lower;
+			water += level - lower; n--;
+        }
+        return water;
+    }
+}
