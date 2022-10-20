5034f84aaf0f8f377bf510bec75ac400d95e4d7a
Rohit Shaw <carpediem212045@gmail.com>
2022-10-20 20:06:11 +0530

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        // Arrays.sort(a);
+        int low = 0, mid = 0, temp;
+        int high = n-1;
+        while(mid <= high){
+            switch(a[mid]){
+                case 0: {
+                    temp = a[low];
+                    a[low] = a[mid];
+                    a[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1:
+                    mid++;
+                    break;
+                case 2: {
+                    temp = a[mid];
+                    a[mid] = a[high];
+                    a[high] = temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+// { Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+  // } Driver Code Ends
