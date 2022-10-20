76f7056836f7c90f08203fbb15d1840956a1d608
Rohit Shaw <carpediem212045@gmail.com>
2022-10-20 14:26:39 +0530

    Time: 1 ms (49.68%), Space: 42.1 MB (64.11%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -30,33 +30,33 @@ class Solution {
         // postorderTraversal(root.right); #1
         // list.add(root.val);  # 1
         
-        helper(root, list);
-        return list;
+        // helper(root, list); # 2
+        // return list;  # 1,2
         
-//         List<Integer> trav = new ArrayList<Integer>();
-//         Stack<TreeNode> pickup = new Stack<TreeNode>();
+        // IteRaTive appRoach !!
+        List<Integer> trav = new ArrayList<Integer>();
+        Stack<TreeNode> pickup = new Stack<TreeNode>();
         
-//         if(root == null) return trav;
-//         TreeNode node = root;
+        if(root == null) return trav;
+        TreeNode node = root;
         
-//         while(node != null || !pickup.isEmpty() ){
-//           if(node != null ){
-//               pickup.push(node);
-//               node = node.left;    
-//           }else{
-//               TreeNode temp = pickup.peek().right;
-//               if(temp == null ){
-//                   temp = pickup.pop();
-//                   trav.add(temp.val);
+        while(node != null || !pickup.isEmpty() ){
+          if(node != null ){
+              pickup.push(node);
+              node = node.left;    
+          }else{
+              TreeNode temp = pickup.peek().right;
+              if(temp == null ){
+                  temp = pickup.pop();
+                  trav.add(temp.val);
                   
-//                   while(!pickup.isEmpty() && temp == pickup.peek().right){
-//                       temp = pickup.pop();
-//                       trav.add(temp.val);
-//                   }
-//               }else
-//                   node = temp;   
-//           }      
-//         }
-//         return trav;
+                  while(!pickup.isEmpty() && temp == pickup.peek().right){
+                      temp = pickup.pop();
+                      trav.add(temp.val);
+                  }
+              }else node = temp;   
+          }      
+        }
+        return trav;
     }
 }
