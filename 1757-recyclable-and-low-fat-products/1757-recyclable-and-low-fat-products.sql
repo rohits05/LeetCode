3c37c18220fe4f80136f7274c99d7b47a6b2f894
Rohit Shaw <carpediem212045@gmail.com>
2022-11-03 23:52:33 +0530

    Time: 1039 ms (13.66%), Space: 0B (100.00%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -2,4 +2,5 @@
 SELECT product_id
 
 FROM Products
-WHERE low_fats IN('Y') AND recyclable IN('Y')
+-- WHERE low_fats IN('Y') AND recyclable IN('Y')
+WHERE low_fats LIKE '%Y' AND recyclable LIKE '%Y'
