0f90510ddf3bef7416fe1c66364add4f9c722a4f
Rohit Shaw <carpediem212045@gmail.com>
2022-11-04 22:31:15 +0530

    Time: 700 ms (67.57%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
-SELECT employee_id,
-IF (employee_id%2
-    AND name not like "M%", salary, 0) 
-    as bonus
--- SELECT employee_id, 
-            --  CASE WHEN employee_id%2=1 
-            -- AND name NOT LIKE 'M%' 
-            --  THEN salary ELSE 0 END bonus
+# SELECT employee_id,
+# IF (employee_id%2
+#     AND name not like "M%", salary, 0) 
+#     as bonus
+SELECT employee_id, 
+            CASE WHEN employee_id%2=1 
+            AND name NOT LIKE 'M%' 
+            THEN salary ELSE 0 END bonus 
+            -- SQL dosen't have if() method
 FROM Employees ORDER BY 1
