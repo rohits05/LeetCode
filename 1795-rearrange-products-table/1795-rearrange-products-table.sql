ea12ad8a9ff2a3a93b1bc600b141222535dcd379
Rohit Shaw <carpediem212045@gmail.com>
2022-11-12 19:54:05 +0530

    Time: 1029 ms (15.24%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,16 +1,19 @@
 # Write your MySQL query statement below
 select product_id, 'store1' as store, 
         store1 as price 
-from Products where store1
+from Products
+where store1 is not NULL
 
 union
 select product_id, 'store2' as store,
         store2 as price 
-from Products where store2
+from Products
+where store2  is not NULL
 
 union 
 select product_id, 'store3' as store,
         store3 as price 
-from Products where store3
+from Products
+where store3  is not NULL
 
 order by 1,2 asc;
