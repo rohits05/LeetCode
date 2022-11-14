91620366b15bc6319fe821fe5be69153816f82c7
Rohit Shaw <carpediem212045@gmail.com>
2022-11-14 07:40:05 +0530

    Time: 687 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-# update salary 
-# set sex = if(sex like 'f%', 'm%', 'f%')
+update salary 
+set sex = if(sex like 'f%', 'm%', 'f%')
 
 # Using Case Statement!
-update Salary
-set sex = 
-        case when sex like 'f%' then 'm%'
-        when sex like 'm%' then 'f%' 
-        end
+# update Salary
+# set sex = 
+#         case when sex like 'f%' then 'm%'
+#         when sex like 'm%' then 'f%' 
+#         end
