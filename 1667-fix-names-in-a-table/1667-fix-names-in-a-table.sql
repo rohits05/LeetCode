105a04050429749af4ae94dcf52dab7c48f69a9f
Rohit Shaw <carpediem212045@gmail.com>
2022-11-15 06:52:56 +0530

    Time: 828 ms (56.90%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
 select user_id, concat(upper(left(name,1)),lower(right(name,length(name)-1))) as name -- Using concat method with specific index pos to get the desired left & right chars
-from Users
-order by 1;-- sorting in ascending order ASC | DESC |
+from Users order by user_id asc;-- sorting in ascending order ASC | DESC |
