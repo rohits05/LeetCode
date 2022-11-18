b28ac3598cf38d4146a422926066deeb100418f7
Rohit Shaw <carpediem212045@gmail.com>
2022-11-18 12:06:00 +0530

    Time: 1552 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-select id,
-case 
-    when p_id is null then 'Root'
-    when id in (select p_id from tree) then 'Inner'
-    else 'Leaf' end as type
-from Tree
-order by 1 asc;
+select id,  
+        if(p_id is null, 'Root',if(id in (select p_id from Tree), 'Inner','Leaf')) as type
+from Tree order by 1;
 
