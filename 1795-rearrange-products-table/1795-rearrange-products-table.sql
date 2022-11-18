c6af5efb509db6120e36ec329ccca4f8af03bbf2
Rohit Shaw <carpediem212045@gmail.com>
2022-11-18 12:34:16 +0530

    Time: 661 ms (89.44%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,7 +1,8 @@
-# Write your MySQL query statement below
-select product_id, 'store1' as store, store1 as price from Products where store1 is not null
-union
-select product_id, 'store2' as store, store2 as price from Products where store2 is not null
-union
-select product_id, 'store3' as store, store3 as price from Products where store3 is not null
-order by 1,2 asc;
+/* Write your T-SQL query statement below */
+select product_id, store, price
+from
+(select product_id, store1, store2, store3
+from Products) p
+UNPIVOT
+(price for store IN (store1, store2, store3)
+) as example;
