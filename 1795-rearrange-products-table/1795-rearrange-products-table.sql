9754dfe9051b5567570169a91af9884ccfa359fb
Rohit Shaw <carpediem212045@gmail.com>
2022-11-18 12:35:00 +0530

    Time: 1585 ms (8.10%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,8 +1,4 @@
 /* Write your T-SQL query statement below */
 select product_id, store, price
-from
-(select product_id, store1, store2, store3
-from Products) p
-UNPIVOT
-(price for store IN (store1, store2, store3)
-) as example;
+from (select product_id, store1, store2, store3 from Products) p
+unpivot (price for store in (store1, store2, store3)) as example;
