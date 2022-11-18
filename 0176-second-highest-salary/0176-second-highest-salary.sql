75bd21db0b77ccaa81260fe6807e349ce059b029
Rohit Shaw <carpediem212045@gmail.com>
2022-11-18 12:45:35 +0530

    Time: 546 ms (7.98%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary from Employee
-where salary not in (select max(salary) from Employee)
+# select max(salary) as SecondHighestSalary from Employee
+# where salary not in (select max(salary) from Employee)
+select max(e.salary) as SecondHighestSalary
+from Employee e
+right join Employee r on e.salary < r.salary
