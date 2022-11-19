0baa0a4c1f42dd0e41806f308d52631d5750cfea
Rohit Shaw <carpediem212045@gmail.com>
2022-11-19 15:22:42 +0530

    Time: 1171 ms (18.43%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 select sD.id from Weather as fD, Weather as sD
-where datediff(sD.recordDate, fD.recordDate)=1 and sD.temperature > fD.temperature;
+where datediff(sD.recordDate, fD.recordDate)=1 and sD.temperature > fD.temperature order by id asc;
