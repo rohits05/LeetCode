b1e919c663826dc6b79d1c7dd1fcb13531a2804a
Rohit Shaw <carpediem212045@gmail.com>
2022-11-20 12:24:36 +0530

    Time: 630 ms (40.05%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select  customer_number from Orders group by customer_number
+select customer_number from Orders group by customer_number
 Order by count(*) desc limit 1;
