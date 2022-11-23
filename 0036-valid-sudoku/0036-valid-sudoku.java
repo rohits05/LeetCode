dea716314feb77cd1e90cd00c0ec40a6e8ab70d4
Rohit Shaw <carpediem212045@gmail.com>
2022-11-23 09:28:13 +0530

    Time: 29 ms (25.41%), Space: 48.2 MB (26.18%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet view = new HashSet();
+        for(int i=0 ; i<9 ; i++){
+            for(int j=0 ; j<9 ; j++){
+                if(board[i][j] != '.'){
+                     if(!view.add("row" + i + board[i][j]) 
+                        || !view.add("column" + j + board[i][j]) 
+                        || !view.add("box" + (i/3)+j/3 + board[i][j]))  return false;
+                }        
+            } 
+        }
+        
+        return true;
+    }
+}
