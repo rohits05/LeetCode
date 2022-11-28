d369f40c4aeaa0af07600ccac245b2fce952ca66
Rohit Shaw <carpediem212045@gmail.com>
2022-11-28 20:17:29 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (91.44%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == target) return true;
+            }
+        }
+        return false;
+        
+        // T-P meThoD !!
+//         int i = 0, j = matrix[0].length - 1;
+//             while (i < matrix.length && j >= 0) {
+//                     if (matrix[i][j] == target) {
+//                         return true;
+//                     } else if (matrix[i][j] > target) {
+//                         j--;
+//                     } else {
+//                         i++;
+//                     }
+//                 }
+            
+//             return false;
+    }
+}
