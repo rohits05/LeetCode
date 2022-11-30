5d1839f3ba75074c15a06ce85717591855496146
Rohit Shaw <carpediem212045@gmail.com>
2022-11-30 23:03:20 +0530

    Time: 9 ms (55.82%), Space: 56.9 MB (10.57%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Moore Voting Algo
+        int els =0, cnt =0;
+        for(int item: nums){
+            if(cnt == 0) els = item;
+            
+            if(item == els) cnt +=1;
+            else cnt -=1;
+        }
+        return els;
+    }
+}
