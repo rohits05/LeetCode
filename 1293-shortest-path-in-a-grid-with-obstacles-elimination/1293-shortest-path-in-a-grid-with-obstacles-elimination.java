8c3b4d83f227f6f8dbdd6a78f30c686ffc3980d5
Rohit Shaw <carpediem212045@gmail.com>
2022-12-05 10:15:26 +0530

    Time: 37 ms (68.23%), Space: 43.6 MB (80.48%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,7 +1,6 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
         if(k >= m+n-2) return m+n-2;
         
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
@@ -11,31 +10,24 @@ class Solution {
         visited[k][0][0] = true;
         var ans = 0;
         
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int size = queue.size();
             for(int i=0; i<size; i++){ 
                 int[] cur = queue.poll();
                 int remainK = cur[0];
-                int x = cur[1];
-                int y = cur[2];
+                int x = cur[1], y = cur[2];
                 
-                if(x == m-1 && y == n-1) return ans;
-                                                
+                if(x == m-1 && y == n-1) return ans;                            
                 for(int[] dir : dirs){
                     int newX = x + dir[0];
                     int newY = y + dir[1];
                     if(newX >= 0 && newX < m && newY >= 0 && newY < n){
                         int newK = remainK;
-                        if(grid[newX][newY] == 1){
-                            newK -= 1;
-                        }
+                        if(grid[newX][newY] == 1) newK -= 1;
                         if(newK >= 0 && visited[newK][newX][newY] == false){
                             visited[newK][newX][newY] = true;
                             queue.offer(new int[] {newK, newX, newY});
-                        }
-                    }
-                }
-            }
+                        }} }}
             ans += 1;
         } 
         return -1;
