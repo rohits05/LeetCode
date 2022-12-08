14c98add513761e4c48d63e4232738ff84e4f43f
Rohit Shaw <carpediem212045@gmail.com>
2022-12-08 21:59:20 +0530

    Time: 1 ms (95.29%), Space: 41 MB (42.37%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -3,8 +3,8 @@ class Solution {
         if(n < 4 || (n > 4 && n % 2 == 0) || (int) Math.sqrt(n) * (int) Math.sqrt(n) <n)
 	        return false;
         else
-	        for (int i = 3; i <= Math.sqrt((int) Math.sqrt(n)); i += 2)
-		    if (n % i == 0) return false;
+	        for(int i=3; i<=Math.sqrt((int) Math.sqrt(n)); i+=2)
+		    if(n % i == 0) return false;
             
 	    return true;
     }
