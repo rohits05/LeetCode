0e28ec26dbdca26f1dc5a3ffa6a4bbe80e044899
Rohit Shaw <carpediem212045@gmail.com>
2022-12-14 20:05:48 +0530

    Time: 1 ms (66.56%), Space: 39 MB (96.22%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findComplement(int num) {
+        // return num ^ ((Integer.highestOneBit(num) << 1) - 1);
+        
+        // BiT MaskinG !1
+        int binaryLen = (int) (Math.log(num) / Math.log(2)) + 1;
+        String s = "";
+        while (binaryLen > 0) {
+          s += '1';
+          binaryLen--;
+        }
+        return Integer.parseInt(s, 2) ^ num;
+    }
+}
