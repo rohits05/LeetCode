ab98d2f65578b65023a81ab25f20009004b382f7
Rohit Shaw <carpediem212045@gmail.com>
2022-12-20 15:01:47 +0530

    Time: 4 ms (42.49%), Space: 45.1 MB (22.03%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean seen[] = new boolean[rooms.size()];
+        seen[0] = true;
+        
+        Stack<Integer> keys = new Stack();
+        keys.add(0);
+        
+        while(!keys.isEmpty()){
+            int curr = keys.pop();
+            for(int new_keys: rooms.get(curr)){
+                if(!seen[new_keys]){
+                    seen[new_keys] = true;
+                    keys.add(new_keys);
+                }
+            }
+        }
+        
+        for(boolean visited: seen) if(!visited) return false;
+        return true;
+    }
+}
