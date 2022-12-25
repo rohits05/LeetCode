4d4cd0af24891292c9f1272c0c37454d88bbc206
Rohit Shaw <carpediem212045@gmail.com>
2022-12-25 11:39:40 +0530

    Time: 11 ms (58.89%), Space: 42.9 MB (85.15%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -2,16 +2,37 @@ class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         Arrays.sort(nums);
 		int[] res = new int[queries.length];
-        // int count =0, sum =0, j=0;
+//         // int count =0, sum =0, j=0;
         
-        for(int i=0, count =0, sum =0, j=0; i<queries.length; i++, count =0, sum =0, j=0){
-            while(j < nums.length && sum + nums[j] <= queries[i]) {
-                sum += nums[j++];
-                count++;
-            }
-            res[i] = count;
+//         for(int i=0, count =0, sum =0, j=0; i<queries.length; i++, count =0, sum =0, j=0){
+//             while(j < nums.length && sum + nums[j] <= queries[i]) {
+//                 sum += nums[j++];
+//                 count++;
+//             }
+//             res[i] = count;
+//         }
+        
+//         return res;
+        
+        // Prefix - Sum Technique !!
+        TreeMap<Integer, Integer> treeMap = createTreeMap(nums);
+        
+        for(int i=0; i<queries.length; i++) {
+            Integer floorKey = treeMap.floorKey(queries[i]);
+            if(floorKey != null) res[i] = treeMap.get(floorKey);
+        }
+
+        return res;
+    }
+    
+    public TreeMap<Integer, Integer> createTreeMap(int[] nums) {
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        
+        for(int i=0, sum=0; i<nums.length; i++){
+            sum += nums[i];
+            treeMap.put(sum, i+1);
         }
         
-        return res;
+        return treeMap;
     }
 }
