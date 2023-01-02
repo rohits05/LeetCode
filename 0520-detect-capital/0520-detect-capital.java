2751c2a5064e3c274c040f4570b1e40da03aa41e
Rohit Shaw <carpediem212045@gmail.com>
2023-01-02 10:33:14 +0530

    Time: 1 ms (100.00%), Space: 40.5 MB (95.10%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() < 2) 
+            return true;
+        if(word.toUpperCase().equals(word)) 
+            return true;
+        if(word.substring(1).toLowerCase().equals(word.substring(1))) 
+            return true;
+        return false;
+    }
+}
