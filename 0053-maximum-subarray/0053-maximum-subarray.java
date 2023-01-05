8d2eb630f5bc153b89b0ef47f31fe9e4e597d7c2
Rohit Shaw <carpediem212045@gmail.com>
2023-01-05 13:27:47 +0530

    Time: 3 ms (18.90%), Space: 72.8 MB (57.81%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+//         int sum = 0;
+//         int maxi = nums[0];   
+//         for(int i=0 ; i<nums.length ; i++){       
+//             sum += nums[i];        
+//             if( sum > maxi)
+//                 maxi = sum;
+            
+//             if( sum < 0)
+//                 sum = 0;
+//         }  
+//         return maxi;
+        
+        // Using,Tabulation DP AppRoach!!
+        int n = nums.length;
+//         int dp[] = new int[n];
+//         int maxI = nums[0];
+//         dp[0] = nums[0];
+        
+//         for(int i=1; i<n; i++){
+//             dp[i] = Math.max(nums[i], nums[i]+dp[i-1]);
+//             maxI = Math.max(maxI, dp[i]);
+//         }
+//         return maxI;
+        
+        // Space Opto=imization!
+        int prev = nums[0];
+        int curr = nums[0];
+        for(int i=1; i<n; i++){
+            prev = Math.max(nums[i], prev + nums[i]);
+            curr = Math.max(curr, prev);
+        }
+        return curr;
+    }
+}
