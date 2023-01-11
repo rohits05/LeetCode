343eb399e75509053909ed396d7440894ad2a9dd
Rohit Shaw <carpediem212045@gmail.com>
2023-01-11 11:00:02 +0530

    Time: 41 ms (86.78%), Space: 50.7 MB (80.07%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
+        // O(NlogN) - O(1)
         int res =0;
         Arrays.sort(nums);
         for(int i=1; i<nums.length; i++){
@@ -9,5 +10,24 @@ class Solution {
             }
         }
         return res;
+        
+        // O(N) - O(N)
+//         int[] arr = new int[100000];
+//         int baggage = 0, increments = 0;
+//         for(int el: nums){ arr[el]++; }
+
+//         for(int i=0; i<100000; i++){
+//             if(arr[i] == 1) continue;
+            
+//             if(arr[i] > 1){
+//                 baggage += arr[i] - 1;
+//                 increments -= i * (arr[i] - 1);
+//             }
+//             else if(baggage > 0 && arr[i] == 0){
+//                 baggage--;
+//                 increments += i;
+//             }
+//         }
+//         return increments;
     }
 }
