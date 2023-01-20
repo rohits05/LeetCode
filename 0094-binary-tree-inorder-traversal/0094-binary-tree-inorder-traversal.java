187d25f22a594c358b54585b0bde061dd15a779b
Rohit Shaw <carpediem212045@gmail.com>
2023-01-20 10:10:07 +0530

    Time: 1 ms (32.73%), Space: 40.7 MB (59.45%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,43 +14,42 @@
  * }
  */
 class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // recursive Approach !!
+        // List<Integer> in = new LinkedList<Integer>();
+        // if(root == null) return in;  # 1
+        
+        // inHelper(root, in);
+        // in.addAll(inorderTraversal(root.left)); # 1
+        // in.add(root.val); # 1
+        // in.addAll(inorderTraversal(root.right)); # 1
+        // return in;
+         
+        // Iterative Approach !!
+        List<Integer> trav = new ArrayList<Integer>();
+        Stack<TreeNode> pickup = new Stack<TreeNode>();
+        TreeNode node = root;
+        
+        while(true){
+            if(node != null){
+                pickup.push(node);
+                node = node.left;    
+            }else{
+                if(pickup.isEmpty()) break;
+                node = pickup.pop();
+                trav.add(node.val);
+                node = node.right;
+            }
+        }
+        
+        return trav;       
+    }
+    
     public void inHelper(TreeNode root, List<Integer> in){
         if(root == null) return; 
         
         inHelper(root.left, in);
         in.add(root.val);
         inHelper(root.right, in);
-    }
-    public List<Integer> inorderTraversal(TreeNode root) {
-        // recursive Approach !!
-        List<Integer> in = new LinkedList<Integer>();
-        // if(root == null) return in;  # 1
-        
-        inHelper(root, in);
-        
-        // in.addAll(inorderTraversal(root.left)); # 1
-        // in.add(root.val); # 1
-        // in.addAll(inorderTraversal(root.right)); # 1
-        return in;
-         
-        // Iterative Approach !!
-//         List<Integer> trav = new ArrayList<Integer>();
-//         Stack<TreeNode> pickup = new Stack<TreeNode>();
-        
-//         TreeNode node = root;
-        
-//         while(true){
-//             if(node != null){
-//                 pickup.push(node);
-//                 node = node.left;    
-//             }else{
-//                 if(pickup.isEmpty()) break;
-
-//                 node = pickup.pop();
-//                 trav.add(node.val);
-//                 node = node.right;
-//             }
-//         }
-//         return trav;       
     }
 }
