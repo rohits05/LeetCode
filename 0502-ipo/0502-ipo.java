3fdf5100dc0262ae7c4db0cd3862204fe312b8c1
Rohit Shaw <carpediem212045@gmail.com>
2023-02-23 11:27:09 +0530

    Time: 114 ms (42.50%), Space: 90.8 MB (12.90%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    class pair {
+    int cap, pro;
+        
+        pair(int cap, int pro) {
+            this.cap = cap;
+            this.pro = pro;
+        }
+    }
+    
+    public int findMaximizedCapital(int k, int w, int[] p, int[] c) {
+        int n =p.length;
+        PriorityQueue<Integer> pq= new PriorityQueue<>((x, y) -> y-x);
+        pair a[] = new pair[n];
+        
+        for(int i=0; i<n; i++)a[i]=new pair(c[i], p[i]);
+        Arrays.sort(a, (x, y) -> x.cap - y.cap);
+        
+        int i =0;
+        while(k-->0){
+            while(i<n && a[i].cap<=w){
+                pq.add(a[i].pro);
+                i++;
+            }
+            if(pq.size() == 0) return w;
+            w += pq.poll();
+        }
+        
+        return w;
+    }
+}
