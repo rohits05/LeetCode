4ec6b19f30d52b24cf5aeea3a2d7297727084045
Rohit Shaw <carpediem212045@gmail.com>
2023-02-28 12:11:25 +0530

    Time: 11 ms (50.92%), Space: 42.9 MB (21.45%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Integer> m1 = new HashMap<>();
-        Map<Character, Integer> m2 = new HashMap<>();
+//         Map<Character, Integer> m1 = new HashMap<>();
+//         Map<Character, Integer> m2 = new HashMap<>();
     
-        for(Integer i=0; i<s.length(); i++) {
-            if(m1.put(s.charAt(i), i) != m2.put(t.charAt(i), i)) return false;
-        }
-        return true;
-        
-//         Map<Character, Character> hm = new HashMap<>();
-        
-//         for(int i=0; i<s.length(); i++){
-//             if(hm.containsKey(s.charAt(i)))
-//                 if(!hm.get(s.charAt(i)).equals(t.charAt(i))) return false; 
-//             else
-//                 if(hm.containsValue(t.charAt(i))) return false;
-//             hm.put(s.charAt(i), t.charAt(i));
+//         for(Integer i=0; i<s.length(); i++) {
+//             if(m1.put(s.charAt(i), i) != m2.put(t.charAt(i), i)) return false;
 //         }
 //         return true;
+        
+        // Optimized!!
+        // int n1 = s.length(), n2 = t.length();
+        // if(n1 != n2) return false;
+        // if(n1 == 1) return true;
+        
+        Map<Character, Character> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i)) != t.charAt(i))
+                return false;
+            }
+            else{
+                if(map.containsValue(t.charAt(i))) return false;
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        
+       return true;
     }
 }
