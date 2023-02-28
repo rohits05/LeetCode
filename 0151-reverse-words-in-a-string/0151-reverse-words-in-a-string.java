36377395ef3ed30e992b9268956a3f17ed286193
Rohit Shaw <carpediem212045@gmail.com>
2023-02-28 10:13:08 +0530

    Time: 19 ms (15.01%), Space: 43.2 MB (17.11%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,23 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-//        String ans ="";
-//        String[] words = s.trim().split(" +");
-//           for(int i=words.length-1; i>=0;i--){
-//               ans += words[i];
-//               if(i != 0) ans += " ";
-//           }
+        Stack<String> st = new Stack<>();
+        String temp ="", ans ="";
         
-//        return ans;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ' '){
+                if(temp.length() > 0) st.push(temp);
+                temp = "";
+            }else temp += s.charAt(i);
+        }
+        ans += temp;
+        
+        while(!st.isEmpty()) ans = ans + " " + st.pop(); // Concat word to ans
+        
+        if(ans.length() != 0 && ans.charAt(0) == ' ') //Trailing sapces
+            ans = ans.substring(1);
+        return ans;
         
         // Using String-Builder!
-        String[] words = s.split(" ");
-        StringBuilder dev = new StringBuilder();
-        for(int i=words.length-1; i>=0; i--) {
-            if(words[i] != "") {
-                dev.append(words[i].trim() + " ");
-            }
-        }
+//         String[] words = s.split(" ");
+//         StringBuilder dev = new StringBuilder();
+//         for(int i=words.length-1; i>=0; i--) {
+//             if(words[i] != "") {
+//                 dev.append(words[i].trim() + " ");
+//             }
+//         }
 
-        return dev.toString().trim();
+//         return dev.toString().trim();
     }
 }
