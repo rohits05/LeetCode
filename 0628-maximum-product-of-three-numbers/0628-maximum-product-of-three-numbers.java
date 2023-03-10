95871e443bc317722a21d8329b02058abd57320d
Rohit Shaw <carpediem212045@gmail.com>
2023-03-10 11:29:33 +0530

    Time: 11 ms (75.56%), Space: 43.8 MB (38.33%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,44 +1,44 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[n-1]*nums[0]*nums[1]);
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // return Math.max(nums[n-1]*nums[n-2]*nums[n-3], nums[n-1]*nums[0]*nums[1]);
         
-//         int max1 = Integer.MIN_VALUE;
-//         int max2 = Integer.MIN_VALUE;
-//         int max3 = Integer.MIN_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
         
-//         int min1 = Integer.MAX_VALUE;
-//         int min2 = Integer.MAX_VALUE;
-//         int min3 = Integer.MAX_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int min3 = Integer.MAX_VALUE;
 
-//         for(int i=0; i<nums.length; i++) {
-//             Integer val = nums[i];
-//             // Finding the 3 Maximum No.s
-//             if(val >= max1) {
-//                 max3 = max2;
-//                 max2 = max1;
-//                 max1 = val;
-//             } else if(val >= max2 && val < max1) {
-//                 max3 = max2;
-//                 max2 = val;
-//             } else if(val >= max3 && val < max2) {
-//                 max3 = val;
-//             }
+        for(int i=0; i<nums.length; i++) {
+            Integer val = nums[i];
+            // Finding the 3 Maximum No.s
+            if(val >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = val;
+            } else if(val >= max2 && val < max1) {
+                max3 = max2;
+                max2 = val;
+            } else if(val >= max3 && val < max2) {
+                max3 = val;
+            }
 
-//             // Finding the 3 Minimum No.s
-//             if(val <= min1) {
-//                 min3 = min2;
-//                 min2 = min1;
-//                 min1 = val;
-//             } else if(val <= min2 && val > min1) {
-//                 min3 = min2;
-//                 min2 = val;
-//             } else if(val <= min3 && val > min2) {
-//                 min3 = val;
-//             }
-//         }
+            // Finding the 3 Minimum No.s
+            if(val <= min1) {
+                min3 = min2;
+                min2 = min1;
+                min1 = val;
+            } else if(val <= min2 && val > min1) {
+                min3 = min2;
+                min2 = val;
+            } else if(val <= min3 && val > min2) {
+                min3 = val;
+            }
+        }
 
-//         return Math.max(max1*max2*max3, max1*min1*min2);
+        return Math.max(max1*max2*max3, max1*min1*min2);
     }
 }
