e6d882675e03e6bff730686e57974335f5afaecc
Rohit Shaw <carpediem212045@gmail.com>
2023-03-23 15:25:26 +0530

    Time: 1 ms (99.04%), Space: 44.9 MB (29.10%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,7 +3,7 @@ class Solution {
         int row = board.length, col = board[0].length;
         if(row <= 2 || col <= 2) return;
 
-        for(int i=0; i<row; i++) {
+        for(int i=0; i<row; i++){
             if(board[i][0] == 'O') dfs(board, i, 0);    
             if(board[i][col - 1] == 'O') dfs(board, i, col - 1);    
         }
@@ -19,8 +19,8 @@ class Solution {
             }
         }
       
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
                 if(board[i][j] == '#') board[i][j] = 'O';
             }
         }
@@ -30,8 +30,8 @@ class Solution {
 	if (board[i][j] == 'O'){
 		board[i][j] = '#';
 		if(i > 1 && board[i - 1][j] == 'O') dfs(board, i - 1, j);
+        if(j > 1 && board[i][j - 1] == 'O') dfs(board, i, j - 1);
 		if(i + 1 < board.length  && board[i + 1][j] == 'O') dfs(board, i + 1, j);
-		if(j > 1 && board[i][j - 1] == 'O') dfs(board, i, j - 1);
 		if(j + 1 < board[0].length && board[i][j + 1] == 'O') dfs(board, i, j + 1);
 	}
   }
