8b8094928b8bbb6abefcd6b8aca03cac19bcf316
Rohit Shaw <carpediem212045@gmail.com>
2023-03-23 16:13:35 +0530

    Time: 9 ms (78.16%), Space: 56.1 MB (38.96%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,40 +1,39 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        // Your code here
-        // int n = grid.length, int m = grid[0].length;
-        // int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-        // boolean[][] vis = new boolean[n][m];
-        // Queue<int[]> q = new LinkedList<>();
-        // //traversing through boundaries
-        // for(int j=0; j<m; j++){
-        //     if(!vis[0][j] && grid[0][j] == 1) q.add(new int[]{0,j});
-        //     if(!vis[n-1][j] && grid[n-1][j] == 1) q.add(new int[]{n-1,j});
-        // }
+//         int n = grid.length, m = grid[0].length;
+//         int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+//         boolean[][] vis = new boolean[n][m];
+//         Queue<int[]> q = new LinkedList<>();
         
-        // for(int i=0;i<n;i++){
-        //     if(!vis[i][0] && grid[i][0] == 1) q.add(new int[]{i,0});
-        //     if(!vis[i][m-1] && grid[i][m-1] == 1) q.add(new int[]{i,m-1});
-        // }
+//         for(int j=0; j<m; j++){  // Boundaries!!
+//             if(!vis[0][j] && grid[0][j] == 1) q.add(new int[]{0,j});
+//             if(!vis[n-1][j] && grid[n-1][j] == 1) q.add(new int[]{n-1,j});
+//         }
         
-        // while(!q.isEmpty()){
-        //     int[] temp = q.poll();
-        //     vis[temp[0]][temp[1]] = true;
-        //     for(int[] d : dir){
-        //         int i = temp[0] + d[0];
-        //         int j = temp[1] + d[1];
-        //         if(i>= 0 && j >= 0 && i <n && j < m && !vis[i][j] && grid[i][j] == 1)
-        //             q.add(new int[]{i,j});
-        //     }
-        // }
+//         for(int i=0; i<n; i++){
+//             if(!vis[i][0] && grid[i][0] == 1) q.add(new int[]{i,0});
+//             if(!vis[i][m-1] && grid[i][m-1] == 1) q.add(new int[]{i,m-1});
+//         }
         
-        // int ans = 0;
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<m; j++){
-        //         if(!vis[i][j] &&grid[i][j] == 1) ans++;
-        //     }
-        // }
+//         while(!q.isEmpty()){
+//             int[] temp = q.poll();
+//             vis[temp[0]][temp[1]] = true;
+//             for(int[] d : dir){
+//                 int i = temp[0] + d[0];
+//                 int j = temp[1] + d[1];
+//                 if(i>= 0 && j >= 0 && i <n && j < m && !vis[i][j] && grid[i][j] == 1)
+//                     q.add(new int[]{i,j});
+//             }
+//         }
         
-        // return ans;
+//         int ans = 0; // ResT portions!!
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(!vis[i][j] &&grid[i][j] == 1) ans++;
+//             }
+//         }
+        
+//         return ans;
         
         // DFS!!
         int n = grid.length, m = grid[0].length;
