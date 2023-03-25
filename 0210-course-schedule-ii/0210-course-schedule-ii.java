fcc27ad196f67902e7b2057ade617134f03aae7d
Rohit Shaw <carpediem212045@gmail.com>
2023-03-25 14:32:56 +0530

    Time: 5 ms (75.92%), Space: 44.2 MB (17.22%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,34 +1,36 @@
 class Solution {
-    int resIdx;
-    boolean dfs(int[] res, Map<Integer, List<Integer>> adjList, int[] state, int course) {
-        if(state[course] == 2) return false;
-        if(state[course] == 1) return true; 
-        state[course] = 1; 
-        
-        if(adjList.containsKey(course))
-          for(int next: adjList.get(course))
-            if(dfs(res, adjList, state, next)) return true;
-
-        state[course] = 2; 
-        res[resIdx--] = course;
-        return false;
-   }
-    
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] state = new int[numCourses];
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        List<List<Integer>> graph = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
         
-        for(int[] pre: prerequisites){
-          int a = pre[0], b = pre[1];
-          adjList.putIfAbsent(b, new ArrayList<>());
-          adjList.get(b).add(a); 
+        for(int i=0; i<numCourses; i++)
+            graph.add(new ArrayList<Integer>());
+        
+        int[] indegree = new int[numCourses];
+        for(int i=0; i<prerequisites.length; i++){
+            int course = prerequisites[i][0], prere = prerequisites[i][1];
+            indegree[course]++;
+            graph.get(prere).add(course);
         }
         
-        int[] res = new int[numCourses];
-        resIdx = numCourses - 1;
         for(int i=0; i<numCourses; i++)
-          if(dfs(res, adjList, state, i)) return new int[] {};
+            if(indegree[i] == 0) queue.offer(i);
         
-        return res;
-     }
-} 
+        int ans[] = new int[numCourses];
+        int count = 0;
+        while(!queue.isEmpty()){
+            Integer prere = queue.poll();
+            
+            ans[count++] = prere;
+            for(Integer course: graph.get(prere)){
+                indegree[course]--;
+                if(indegree[course] == 0) queue.offer(course);
+            }
+        }
+        // return (count == numCourses); linear simple order- ~~ CS - 1!1
+        if(count == numCourses) return ans;
+        
+        int ar[]={};
+        return ar;
+    }
+}
