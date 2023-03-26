f037da72956143e11a6d4c4689f5c64330f8a726
Rohit Shaw <carpediem212045@gmail.com>
2023-03-26 15:16:26 +0530

    Time: 40 ms (69.50%), Space: 50.7 MB (99.23%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,56 +1,38 @@
 class Solution {
-    public static void dfs(int node, HashMap<Integer, ArrayList<Integer>> adj, Stack<Integer> stack, boolean[] isVis){
-        isVis[node] = true;
-        for(int it: adj.get(node))
-            if(!isVis[it]) dfs(it, adj, stack, isVis);
-        
-        stack.push(node);
-    }
-    
-    public static void revDfs(int node, HashMap<Integer, ArrayList<Integer>> transpose, int[] scc, boolean[] isVis){
-        isVis[node] = true;
-        scc[0]++;
-        for(int it: transpose.get(node))
-            if(!isVis[it]) revDfs(it, transpose, scc, isVis);
-    }
-    
     public int longestCycle(int[] edges) {
-        int n = edges.length;
-        HashMap<Integer, ArrayList<Integer>> adj = new HashMap();
-        for(int i=0; i<n; i++)
-            adj.put(i, new ArrayList());
+        int answer = -1;
         
-        for(int i=0; i<edges.length; i++)
-            if(edges[i] != -1) adj.get(i).add(edges[i]);
-        
-        boolean[] isVis = new boolean[n];
-        Stack<Integer> stack = new Stack();
-        for(int i=0; i<n; i++){
-            if(!isVis[i]) dfs(i, adj, stack, isVis);
-        }
-        
-        HashMap<Integer, ArrayList<Integer>> transpose=new HashMap<>();
-        
-        for(int i=0; i<n; i++)
-            transpose.put(i, new ArrayList());
-        
-        for(int i=0; i<n; i++){
-            isVis[i] = false;
-            if(edges[i] != -1)
-            transpose.get(edges[i]).add(i);
-        }
-        
-        int res = 0;
-        while(!stack.isEmpty()){
-            int node = stack.pop();
-            if(!isVis[node]){
-                int[] count = new int[1];
-                count[0] = 0;
-                revDfs(node, transpose, count, isVis);
-                res=Math.max(res, count[0]);
+        for(int i=0; i<edges.length; i++){
+            int node =i, hare =edges[node], tortoise =node, cycleSize =1;
+            
+            while(tortoise != -1 && hare != -1){
+                if(hare == tortoise){
+                    tortoise = hare;
+                    hare = edges[hare];
+                    while(hare != tortoise){
+                        cycleSize++;
+                        tortoise = edges[tortoise];
+                        hare = edges[hare];
+                        hare = edges[hare];
+                    }
+
+                    if(answer < cycleSize) answer = cycleSize;
+                    break;
+                }else{
+                    tortoise = edges[tortoise];
+                    hare = edges[hare];
+                    if(hare != -1) hare = edges[hare];
+                }
+            }
+
+            int tmp =i, tmp2 =edges[i];
+            while(tmp!=-1){
+                tmp2 = edges[i];
+                edges[tmp] = -1;
+                tmp = tmp2;
             }
         }
         
-        return res==1?-1:res;
+        return answer;
     }
 }
