a80b79084f0639457f34d7bcd3c3264f30d1a47c
Rohit Shaw <carpediem212045@gmail.com>
2023-04-01 10:26:10 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (47.04%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low =0, high =nums.length-1, mid =low+(high-low)/2;
+        
+        while(low <= high){
+            mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid+1;
+            else high = mid-1;
+        }
+        
+        return -1;
+    }
+}
