b29faeaf669c6a7a5cc7e6daf2991330c4372a3f
Rohit Shaw <carpediem212045@gmail.com>
2023-04-10 16:27:43 +0530

    Time: 2 ms (79.07%), Space: 40.6 MB (52.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,6 +5,7 @@ class Solution {
         for(int i=0; i<s.length(); i++) {
             char a = s.charAt(i);
             if(a == '(' || a == '[' || a == '{')  stack.push(a);
+            
             else if(stack.empty())  return false;
             else if(a == ')' && stack.pop() != '(')  return false;
             else if(a == ']' && stack.pop() != '[')  return false;
