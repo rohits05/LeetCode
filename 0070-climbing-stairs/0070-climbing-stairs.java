080e0ee2b811670a1e974bc7ef40567e86cf53e0
Rohit Shaw <carpediem212045@gmail.com>
2023-04-10 18:32:02 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (55.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
         if(n <= 1) return 1;
-        // Tabulation TechniQue!!
-//         int dp[] = new int[n+1];
-//         dp[1] =1; dp[2] =2;
         
-//         for(int i=3; i<=n; i++) dp[i] = dp[i-1] + dp[i-2];
-//         return dp[n];
-        
-        // Space Optimization!!
-        int prev =1, curr =2;
+        int prev =1, cur =2;
         for(int i=3; i<=n; i++){
-            int next = prev + curr;
-            prev = curr;
-            curr = next;
+            int get = prev + cur;
+            prev = cur; cur = get;
         }
-        return curr;
+        return cur;
     }
 }
