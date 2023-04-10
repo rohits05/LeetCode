e01444748e1aafd0a5b0beeb5aa9e520ebe2b4ac
Rohit Shaw <carpediem212045@gmail.com>
2023-04-10 18:11:05 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (54.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int fib(int n) {
         if(n <= 1) return n;
-        int p2 = 0, p1 = 1; // Spcae optimized!1
+        int p2 = 0, p1 = 1; // Space opTimizeD!!
         for(int i=2; i<=n; i++){
             int cur = p1 + p2;
             p2 = p1; p1 = cur;
