ab7e51ec2e97e18dffd8ad6659265f892bbcca33
Rohit Shaw <carpediem212045@gmail.com>
2023-04-12 17:23:30 +0530

    Time: 8 ms (19.67%), Space: 42.3 MB (41.84%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> grid) {
+         for(int i=grid.size()-2; i>=0; i--){
+            for(int j=0; j<grid.get(i).size(); j++) 
+                grid.get(i).set(j, grid.get(i).get(j) 
+                + Math.min(grid.get(i + 1).get(j), grid.get(i + 1).get(j + 1)));
+        }
+        return grid.get(0).get(0);
+    }
+}
