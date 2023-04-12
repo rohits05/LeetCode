503577d4643eeb85bb58caee56a36136e02ffcd9
Rohit Shaw <carpediem212045@gmail.com>
2023-04-12 16:24:50 +0530

    Time: 3 ms (27.59%), Space: 45 MB (87.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,16 +1,41 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m =grid.length, n =grid[0].length;
+        int n =grid.length, m =grid[0].length;
         
-        if(grid == null || m == 0 || grid[0] == null || n == 0) 
-            return Integer.MIN_VALUE;
-        for(int i=1; i<m; i++) grid[i][0] = grid[i][0] + grid[i-1][0];
-        for(int j=1; j<n; j++) grid[0][j] = grid[0][j] + grid[0][j-1];
+//         if(grid == null || m == 0 || grid[0] == null || n == 0) 
+//             return Integer.MIN_VALUE;
+//         for(int i=1; i<m; i++) grid[i][0] = grid[i][0] + grid[i-1][0];
+//         for(int j=1; j<n; j++) grid[0][j] = grid[0][j] + grid[0][j-1];
         
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++) 
-                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+//         for(int i=1; i<m; i++){
+//             for(int j=1; j<n; j++) 
+//                 grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+//         }
+//         return grid[m - 1][n - 1];
+        
+        // Spcae OptimizaTion !1
+        int[] prev = new int[m];
+        Arrays.fill(prev, 0);
+        
+        for(int i=0; i<n; i++){
+            int[] curr = new int[m];
+            Arrays.fill(curr, 0);
+            for(int j=0; j<m; j++){
+                if(i == 0 && j == 0) curr[j] = grid[i][j];
+                else{
+                    int up = grid[i][j];
+                    if(i > 0) up += prev[j];
+                    else up += 100000;
+
+                    int left = grid[i][j];
+                    if(j > 0) left += curr[j-1];
+                    else left += 100000;
+
+                    curr[j] = Math.min(up, left);
+                }
+            }
+            prev = curr;
         }
-        return grid[m - 1][n - 1];
+        return prev[m-1];
     }
 }
