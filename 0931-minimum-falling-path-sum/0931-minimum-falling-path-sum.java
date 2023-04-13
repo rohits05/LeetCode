bd35f5dff385b1d09de5e7f7ae68e74095fa64ed
Rohit Shaw <carpediem212045@gmail.com>
2023-04-13 10:24:35 +0530

    Time: 5 ms (53.14%), Space: 42.9 MB (79.17%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,26 +3,34 @@ class Solution {
         int n =matrix.length, m =matrix[0].length;
         int maxI = Integer.MAX_VALUE;
         
-        int[][] dp = new int[n][m];
+        // int[][] dp = new int[n][m]; // TabulatIon !
+        int prev[] = new int[n]; // Space . OpTimization !
         
-        for(int i=0; i<m; i++) dp[0][i] = matrix[0][i]; // Base ~ Case !
+        for(int i=0; i<m; i++) prev[i] = matrix[0][i];
+            // dp[0][i] = matrix[0][i]; // Base ~ Case !
         
         for(int i=1; i<n; i++){
+            int cur[] = new int[n];
             for(int j=0; j<m; j++){
-                int up = matrix[i][j] + dp[i - 1][j],
+                int up = matrix[i][j] + prev[j],
+                    // matrix[i][j] + dp[i - 1][j],
                 left = matrix[i][j], right = matrix[i][j]; // bottom - up moves !
                 
-                if(j> 0) left += dp[i - 1][j - 1]; // OuT of BounD Case ~ leftwards
+                if(j> 0) left += prev[j-1];
+                    // dp[i - 1][j - 1]; // OuT of BounD Case ~ leftwards
                 else left = (int)Math.pow(10,9);
                 
-                if(j < n - 1) right += dp[i - 1][j + 1]; // OuT of BounD Case ~ rightwards
+                if(j < n-1) right += prev[j+1];
+                    // dp[i - 1][j + 1]; // OuT of BounD Case ~ rightwards
                 else right = (int)Math.pow(10,9);
                 
-                dp[i][j] = Math.min(up, Math.min(left, right));
+                cur[j] = Math.min(up, Math.min(left, right));
             }
+            prev = cur;
         }
         
-        for(int i=0; i<m; i++) maxI = Math.min(maxI, dp[m - 1][i]);
+        for(int i=0; i<m; i++) maxI = Math.min(maxI, prev[i]);
+                                        // Math.min(maxI, dp[m - 1][i]);
         return maxI;
     }
 }
