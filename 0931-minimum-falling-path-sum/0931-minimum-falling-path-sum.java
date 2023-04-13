69261543637cc53b88584a621a5522e510fe1d2e
Rohit Shaw <carpediem212045@gmail.com>
2023-04-13 10:18:33 +0530

    Time: 6 ms (26.78%), Space: 42.9 MB (71.58%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,28 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        int result = Integer.MAX_VALUE;
+        int n =matrix.length, m =matrix[0].length;
+        int maxI = Integer.MAX_VALUE;
         
-        int[] dp = new int[n];
-        for(int i=0; i<n; i++) dp[i] = matrix[0][i];
+        int[][] dp = new int[n][m];
         
-        for(int i=1; i<m; i++){
-            int[] temp = new int[n];
-            for(int j=0; j<n; j++){
-                int up = matrix[i][j] + dp[j], left = matrix[i][j], right = matrix[i][j];
+        for(int i=0; i<m; i++) dp[0][i] = matrix[0][i]; // Base ~ Case !
+        
+        for(int i=1; i<n; i++){
+            for(int j=0; j<m; j++){
+                int up = matrix[i][j] + dp[i - 1][j],
+                left = matrix[i][j], right = matrix[i][j]; // bottom - up moves !
                 
-                if(j > 0) left += dp[j - 1];
+                if(j> 0) left += dp[i - 1][j - 1]; // OuT of BounD Case ~ leftwards
                 else left = (int)Math.pow(10,9);
                 
-                if(j < n - 1) right += dp[j + 1];
+                if(j < n - 1) right += dp[i - 1][j + 1]; // OuT of BounD Case ~ rightwards
                 else right = (int)Math.pow(10,9);
                 
-                temp[j] = Math.min(up, Math.min(left, right));
+                dp[i][j] = Math.min(up, Math.min(left, right));
             }
-            dp = temp;
         }
         
-        for(int i=0; i<n; i++) result = Math.min(result, dp[i]);
-        return result;
+        for(int i=0; i<m; i++) maxI = Math.min(maxI, dp[m - 1][i]);
+        return maxI;
     }
 }
