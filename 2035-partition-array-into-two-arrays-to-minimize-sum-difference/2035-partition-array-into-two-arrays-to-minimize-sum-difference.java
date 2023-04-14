f4c3715a27c129b85ee382e5541fc5ae1baa8332
Rohit Shaw <carpediem212045@gmail.com>
2023-04-14 09:42:27 +0530

    Time: 1078 ms (36.15%), Space: 51.5 MB (15.96%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,52 +1,38 @@
 class Solution {
-    public void solve(int []nums,int i, int sum, int count, int high, HashMap<Integer, ArrayList<Integer>> hm){
-        if(i == high){
-            hm.get(count).add(sum);
-            return;
-        } 
-        
-        solve(nums, i+1, sum+nums[i], count+1, high, hm);
-        solve(nums, i+1, sum, count, high, hm);
-    }
-    
-     public int minimumDifference(int[] nums) {
-        int total= 0;
-        for(int i: nums) total+=i;
+    HashMap<Integer, TreeSet<Integer>> leftMap = new HashMap<>();
+    HashMap<Integer, TreeSet<Integer>> rightMap = new HashMap<>();
 
-        int target = total/2, min = Integer.MAX_VALUE, val = min;
-        HashMap<Integer, ArrayList<Integer>> hm1 = new HashMap<>();
-        HashMap<Integer, ArrayList<Integer>> hm2 = new HashMap<>();
-         
-        int half = nums.length/2;
-        for(int i=0; i<=half; i++){
-            hm1.put(i, new ArrayList<Integer>());
-            hm2.put(i, new ArrayList<Integer>());
-        }
-         
-        solve(nums, 0, 0, 0, half, hm1);
-        solve(nums, half, 0, 0, nums.length, hm2);
-         
-        for(int i=0; i<=half; i++){
-            ArrayList<Integer> list1 = hm1.get(i);
-            ArrayList<Integer> list2 = hm2.get(half-i);
-            Collections.sort(list1);
-            Collections.sort(list2);
-            int j = 0 , k = list2.size()-1;
-            while(j<list1.size() && k>=0){
-                int sum = list1.get(j) + list2.get(k);
-                if(sum==target) return Math.abs(total-2*sum);
-                else if(sum>target) k--;
-                else j++;
-                
-                int diff = Math.abs(sum-target);
-                if(diff<min){
-                    min = diff;
-                    val = sum;
+    public int minimumDifference(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        subsets(0, Arrays.copyOfRange(nums, 0, nums.length / 2), 0, 0, leftMap);
+        subsets(0, Arrays.copyOfRange(nums, nums.length / 2, nums.length), 0, 0, rightMap);
+        int ans = Integer.MAX_VALUE;
+        for(int len: leftMap.keySet()){
+            int rightArrayLen = nums.length / 2 - len;
+            TreeSet<Integer> right = rightMap.get(rightArrayLen);
+            if (right != null) {
+                for(int leftSum: leftMap.get(len)){
+                    int rightSum = sum / 2 - leftSum;
+                    Integer closest = right.floor(rightSum);
+                    if(closest != null){
+                        int oneSum = (closest + leftSum);
+                        int otherSum = sum - oneSum;
+                        ans = Math.min(ans, Math.abs(oneSum - otherSum));
+                    }
                 }
             }
-            
         }
-         
-        return Math.abs(total - 2 * val);
+        return ans;
+    }
+
+    private void subsets(int idx, int[] arr, int sum, int length, HashMap<Integer, TreeSet<Integer>> map) {
+        if(idx >= arr.length){
+            TreeSet<Integer> set = map.getOrDefault(length, new TreeSet<>());
+            set.add(sum);
+            map.put(length, set);
+            return;
+        }
+        subsets(idx + 1, arr, sum, length, map);
+        subsets(idx + 1, arr, sum + arr[idx], length + 1, map);
     }
 }
