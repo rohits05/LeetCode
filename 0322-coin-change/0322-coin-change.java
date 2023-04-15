604a021f2dc846669b8663d892c7b6c940e54109
Rohit Shaw <carpediem212045@gmail.com>
2023-04-15 12:09:59 +0530

    Time: 14 ms (81.48%), Space: 42.2 MB (64.44%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount < 1) return 0;
+        
+        int L =coins.length, sum =0;
+        int[] dp = new int[amount+1];
+
+        Arrays.sort(coins);
+        while(++sum<=amount){
+            int min = -1;
+            
+            for(int coin: coins){
+                if(sum < coin) break;
+                if(dp[sum-coin]!=-1){
+                    int temp = dp[sum-coin]+1;
+                    min = min<0 ? temp : (temp < min ? temp : min);
+                }
+            }
+            dp[sum] = min;
+        }
+        
+        return dp[amount];
+    }
+}
