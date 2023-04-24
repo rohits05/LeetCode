c253723e466569f87cd6b6cb67ee4cfb10b2ed39
Rohit Shaw <carpediem212045@gmail.com>
2023-04-24 10:55:34 +0530

    Time: 5 ms (63.04%), Space: 40.2 MB (84.58%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,34 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1] ;
-        for(int i=0; i<n; i++) dp[i][0] = 1;
+        // int[][] dp = new int[n][amount+1];
+        
+        // Space opTimizeD !1
+        int prev[] = new int [amount+1];
+        // for(int i=0; i<n; i++) prev[i] = 1;
+            // dp[i][0] = 1;
         
         for(int j=0; j<=amount; j++){
-            if(j % coins[0] == 0) dp[0][j] = 1;
-            else dp[0][j] = 0;
+            if(j % coins[0] == 0) prev[j] = 1;
+                // dp[0][j] = 1;
+            else prev[j] = 0;
+                // dp[0][j] = 0;
         }
         
         for(int i=1; i<n; i++){
-             for(int j=1; j<=amount; j++){
-                 int nT =dp[i-1][j], t =0;
+             for(int j=0; j<=amount; j++){
+                 int nT = prev[j],
+                     // dp[i-1][j], 
+                 t =0;
                  
-                 if(coins[i] <= j) t = dp[i][j-coins[i]];
-                 dp[i][j] =  nT + t;
+                 if(coins[i] <= j) t = prev[j - coins[i]];
+                     // dp[i][j-coins[i]];
+                 prev[j] = nT + t;
+                 // dp[i][j] =  nT + t;
              }
          }
                     
-        return dp[n-1][amount];
+        return prev[amount];
+        // return dp[n-1][amount];
     }
 }
