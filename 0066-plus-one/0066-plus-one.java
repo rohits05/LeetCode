ef8c164cdefdd7f2485b941819794df8fb7e86cb
Rohit Shaw <carpediem212045@gmail.com>
2023-04-25 15:34:54 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (9.36%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,13 +1,12 @@
 class Solution {
-    public int[] plusOne(int[] digits) {
-        int len = digits.length; 
-        for(int i = len-1; i>=0; i--){
-            if(digits[i] == 9) digits[i] = 0;
-            else{ digits[i] += 1; return digits; }     
+    public int[] plusOne(int[] digits) {        
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i] < 9){ digits[i]++; return digits; }
+            digits[i] = 0;
         }
-        
-        int[] arr = new int[len+1];
-        arr[0] = 1;
-        return arr;
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
