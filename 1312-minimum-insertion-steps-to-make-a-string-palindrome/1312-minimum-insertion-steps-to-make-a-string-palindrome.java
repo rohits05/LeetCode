f40c295917c6e1886c3f3347c400c777b9f86e5a
Rohit Shaw <carpediem212045@gmail.com>
2023-04-25 14:09:05 +0530

    Time: 38 ms (41.59%), Space: 43.6 MB (64.73%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,16 +1,12 @@
 class Solution {
-    public int minInsertions(String s) {
-        return (s.length() - lps(s)); // a c p c a
-    }
-    
-    public int lps(String s1){
+    public int minInsertions(String s1) {
         String s2 = "";
         int x =s1.length();
         for(int i=x-1; i>=0; i--) s2 += s1.charAt(i);
         
         int dp[] = new int[x+1];
         
-        for(int i=1; i<=x; i++){
+        for(int i=1; i<=x; i++){ // L.P.S ~
             int cur[] = new int[x+1];
             for(int j=1; j<=x; j++){
                 if(s1.charAt(i-1) == s2.charAt(j-1)) cur[j] = 1 + dp[j-1];
@@ -19,6 +15,6 @@ class Solution {
             dp = cur;
         }
         
-        return dp[x];
+        return (s1.length() - dp[x]); // a c p c a
     }
 }
