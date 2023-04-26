0211032a6bfa57f07ef574993a4f8caaa7d13645
Rohit Shaw <carpediem212045@gmail.com>
2023-04-26 10:24:17 +0530

    Time: 5 ms (78.04%), Space: 42.8 MB (38.74%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,18 +2,24 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int n = s1.length(), m = s2.length();
         
-        int p[][] = new int[n+1][m+1]; 
-        // int c[] = new int[n+1];
-        for(int i=0; i<=n; i++) p[i][0] = i;
-        for(int j=0; j<=m; j++) p[0][j] = j;
+        // int p[][] = new int[n+1][m+1]; 
+        int p[] = new int[m+1];
+        for(int i=1; i<=m; i++) p[i] = i;
+        // for(int j=0; j<=m; j++) p[0][j] = j;
         
         for(int i=1; i<=n; i++){
+            int c[] = new int[m+1];
+            c[0] = i;
             for(int j=1; j<=m; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)) p[i][j] = p[i-1][j-1];
-                else p[i][j] = 1 + Math.min(p[i-1][j], Math.min(p[i][j-1], p[i-1][j-1]));
+                if(s1.charAt(i-1) == s2.charAt(j-1)) c[j] = p[j-1];
+                else{
+                    int ins =p[j], del =c[j-1];
+                    int rep = p[j-1];
+                    c[j] = 1 + Math.min(rep, Math.min(del, ins));
+                }
             }        
-            // p = c;
+            p = c;
         }
-        return p[n][m];
+        return p[m];
     }
 }
