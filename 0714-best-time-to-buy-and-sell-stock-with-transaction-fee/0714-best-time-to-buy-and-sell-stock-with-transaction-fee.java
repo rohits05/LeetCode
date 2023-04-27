b359da7fcb2bef8454fdb7ed46b2f15bae756551
Rohit Shaw <carpediem212045@gmail.com>
2023-04-27 12:34:59 +0530

    Time: 4 ms (99.33%), Space: 50.8 MB (70.01%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int aNB =0, aB =0;
+        int aNB =0, aB =0; // B&SS-II Modification !1
         int cB, cNB;
         
         for(int i=prices.length-1; i>=0; i--){
@@ -9,6 +9,7 @@ class Solution {
             
             aB = cB; aNB = cNB;
         }
+        
         return aB;
     }
 }
