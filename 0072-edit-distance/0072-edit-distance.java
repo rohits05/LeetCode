77d91703719e852352161f5c29f42a2ffdb83fb6
Rohit Shaw <carpediem212045@gmail.com>
2023-04-28 18:08:52 +0530

    Time: 5 ms (78.61%), Space: 42.7 MB (52.59%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -10,16 +10,19 @@ class Solution {
         for(int i=1; i<=n; i++){
             int c[] = new int[m+1];
             c[0] = i;
+            
             for(int j=1; j<=m; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)) c[j] = p[j-1];
+                int je = j-1, ie = i-1;
+                if(s1.charAt(ie) == s2.charAt(je)) c[j] = p[je];
                 else{
-                    int ins =p[j], del =c[j-1];
-                    int rep = p[j-1];
+                    int ins =p[j], del =c[je];
+                    int rep = p[je];
                     c[j] = 1 + Math.min(rep, Math.min(del, ins));
                 }
             }        
             p = c;
         }
+        
         return p[m];
     }
 }
