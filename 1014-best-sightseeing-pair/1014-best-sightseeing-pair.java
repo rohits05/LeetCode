d4debeb8806bafb0c9da91546fc20230e6bd96a1
Rohit Shaw <carpediem212045@gmail.com>
2023-04-28 18:04:32 +0530

    Time: 4 ms (29.62%), Space: 51.9 MB (5.32%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,19 +1,11 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int max =0, n =values.length;
-        int[] posSum = new int[n];
-        int[] negSum = new int[n]; 
-        posSum[0] = values[0];
-        
-        for(int i=1; i<n;i++){
-            posSum[i] = Math.max(values[i] + i, posSum[i-1]);
-            negSum[i] = values[i] - i;
-        }
+        int n = values.length;
+        int max = Integer.MIN_VALUE, currentOptimal = values[0];
 
-        int j =1;
-        while(j < n){
-            max = Math.max(max, negSum[j] + posSum[j-1]);
-            j++;
+        for(int i=1; i<n; i++){
+            currentOptimal = Math.max(currentOptimal-1, values[i-1]-1);
+            max = Math.max(max, currentOptimal + values[i]);
         }
 
         return max;
