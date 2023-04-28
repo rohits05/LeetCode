5680aeae41e95d9085ec8e13b0f97410f3f5b329
Rohit Shaw <carpediem212045@gmail.com>
2023-04-28 07:46:05 +0530

    Time: 170 ms (12.98%), Space: 42.5 MB (29.77%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -0,0 +1,62 @@
+class Solution {
+    HashMap<String, String> parent;
+    HashMap<String, Integer> rank;
+    
+    public int numSimilarGroups(String[] strs) {
+        parent = new HashMap<>();
+        rank = new HashMap<>();
+        
+        for(String str: strs){
+            parent.put(str, str);
+            rank.put(str, 0);
+        }
+        
+        for(int i=0; i<strs.length; i++){
+            for(int j=i+1; j<strs.length; j++){
+                String si = strs[i];
+                String sj = strs[j];
+                
+                if(areSimilar(si, sj)){
+                    String li = find(si);
+                    String lj = find(sj);
+                    
+                    if(li.equals(lj) == false) union(li, lj);
+                }
+            }
+        }
+        
+        int count =0;
+        
+        for(String key: parent.keySet()){
+            if(parent.get(key).equals(key)) count++;
+        }
+        return count;
+    }
+    
+    boolean areSimilar(String x, String y){
+        int res =0;
+        for(int i=0; i<x.length(); i++){
+            if(x.charAt(i) != y.charAt(i)) res++;
+        }
+        return res==2 || res==0;
+    }
+    
+    public String find(String x){
+        if(parent.get(x).equals(x)) return x;
+        else{
+            String pox = parent.get(x);
+            String lead = find(pox);
+            parent.put(x, lead);
+            return lead;
+        }
+    }
+    
+    public void union(String x, String y){
+        if(rank.get(x) < rank.get(y)) parent.put(x, y);
+        else if(rank.get(y) < rank.get(x)) parent.put(y, x);
+        else{
+            parent.put(y, x);
+            rank.put(x, rank.get(x) + 1);
+        }
+    }
+}
