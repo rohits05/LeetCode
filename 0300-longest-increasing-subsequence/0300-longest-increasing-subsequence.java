a25addf9daa7bd625651b9e3e4c4adaeb5a68bf1
Rohit Shaw <carpediem212045@gmail.com>
2023-04-28 11:54:41 +0530

    Time: 2 ms (99.16%), Space: 42.4 MB (51.19%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,34 @@
 class Solution {
     public int lengthOfLIS(int[] a) {
-        int n =a.length;
-        // int dp[][] = new int[n+1][n+1];
-        int next[] = new int[n+1]; int cur[] = new int[n+1];
+           // Space Optimized !
+//         int n =a.length;
+//         // int dp[][] = new int[n+1][n+1];
+//         int next[] = new int[n+1]; int cur[] = new int[n+1];
         
-        for(int i=n-1; i>=0; i--){
-            for(int j=i-1; j>=-1; j--){
-                int len = 0 + next[j+1];
-                if(j == -1 || a[i] > a[j]) len = Math.max(len, 1 + next[i+1]);
+//         for(int i=n-1; i>=0; i--){
+//             for(int j=i-1; j>=-1; j--){
+//                 int len = 0 + next[j+1];
+//                 if(j == -1 || a[i] > a[j]) len = Math.max(len, 1 + next[i+1]);
                 
-                cur[j+1] = len;
-            }
-            next = cur;
+//                 cur[j+1] = len;
+//             }
+//             next = cur;
+//         }
+        
+//         return next[-1+1];
+        
+        // Binary Srch !!
+        int dp[] = new int[a.length];
+        int len = 0;
+        for(int num: a){
+            int i = Arrays.binarySearch(dp, 0, len, num); // 
+            if(i < 0) i = -(i + 1);
+            
+            dp[i] = num;
+            if(i == len) len++;
         }
         
-        return next[-1+1];
+        return len;
     }
 }
+
