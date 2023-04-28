b06040a722e6156257d8c617684b5976ffa33071
Rohit Shaw <carpediem212045@gmail.com>
2023-04-28 17:58:24 +0530

    Time: 4 ms (54.67%), Space: 60.9 MB (5.19%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int getMaxLen(int[] arr) {
-        int start =0, len =0, n= arr.length;
-        boolean pos = true;
+    public int getMaxLen(int[] nums) {
+        int n =nums.length;
+        int pos[ ] =new int[n];
+        int neg[] = new int[n];
+        if(nums[0] < 0) neg[0] += 1;
+        else if(nums[0] > 0) pos[0] += 1;
         
-        for(int end=0; end<=n; end++){   
-            if(end != n && arr[end] != 0){
-                if(arr[end] < 0) pos = !pos;
-                if(pos) len = Math.max(len, end-start+1);
+        int res = pos[0];
+        for(int i=1; i<n; i++){
+            if(nums[i] == 0) continue;
+            if(nums[i] < 0){
+                if(neg[i-1] == 0) pos[i] = 0;
+                else pos[i] = neg[i-1] + 1;
+                neg[i] = pos[i-1] + 1;
+            }else{
+                pos[i] = pos[i-1] + 1;
+                if(neg[i-1] == 0) neg[i] = 0;
+                else neg[i] = neg[i-1] + 1;
             }
-            else{
-                while(start < end){
-                    if(arr[start] < 0) pos = !pos;
-                    if(pos) len = Math.max(len, end-start-1);
-                    start++;
-                }
-                start = end+1;
-                pos = true;
-            }}
+            res  =Math.max(res, pos[i]);
+        }
         
-        return len;
+        return res;
     }
 }
