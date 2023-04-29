23009c96a0c1d22ebb05714fa1ffbab6fc08a82b
Rohit Shaw <carpediem212045@gmail.com>
2023-04-29 18:45:59 +0530

    Time: 494 ms (5.02%), Space: 43 MB (23.08%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,34 +1,41 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Arrays.sort(words, new Comparator<String>(){
-            public int compare(String a, String b){
-                return a.length()-b.length();
+        int n =words.length;
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int [] dp = new int[n];
+        dp[0] = 1;
+        int ans =1;
+
+        for(int i=1; i<n; i++){
+            int max = 0;
+            
+            for(int j=0; j<i; j++){
+                String word1 = words[i];
+                String word2 = words[j];
+                if(word1.length() - word2.length() == 1){
+                    if((lcs(word1, word2) == word2.length()) && dp[j] > max)
+                        max = dp[j];
+                }
             }
-        });
-        
-        int[] dp=new int[words.length];
-        int res=0;
-        for(int i=0;i<words.length;i++){
-            dp[i]=1;
-            for(int j=i-1;j>=0;j--){
-                if(words[j].length()<words[i].length()-1) 
-                    break;
-                if(words[j].length()==words[i].length()||!check(words[i], words[j])) 
-                    continue;
-                dp[i]=Math.max(dp[i], dp[j]+1);  
-            }
-            res=Math.max(res, dp[i]);
+            dp[i] = max + 1;
+            ans = Math.max(dp[i], ans);
         }
-        return res;
+        
+        return ans;
     }
     
-    public boolean check(String a, String b) {
-        for(int i=0, j=0;i<a.length()&&j<b.length();i++, j++){
-            if(a.charAt(i)!=b.charAt(j)&&i==j) 
-                j--;
-            else if(a.charAt(i)!=b.charAt(j)) 
-                return false;
+    int lcs(String s,String t){
+        int n =s.length(), m =t.length();
+        
+        int [][] dp = new int[n+1][m+1];
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                char l = s.charAt(i), r = t.charAt(j);
+                if(l == r) dp[i][j] = dp[i+1][j+1] + 1; 
+                else dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+            }
         }
-        return true;
+        
+        return dp[0][0];
     }
 }
