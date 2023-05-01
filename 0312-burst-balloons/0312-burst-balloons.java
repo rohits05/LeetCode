1744ce2e06f5464b47916875df0f365e4d03ec34
Rohit Shaw <carpediem212045@gmail.com>
2023-05-01 12:19:36 +0530

    Time: 55 ms (69.94%), Space: 42.4 MB (54.83%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int maxCoins(int[] nums) {
-        int n = nums.length;
-        int arr[] = new int[n+2];
-        arr[0] = arr[n+1] = 1; 
-        
-        for(int i=1; i<=n; i++) arr[i] = nums[i-1]; 
-        
-        int dp[][] = new int[n+2][n+2];
-        for(int window=1; window<=n; window++){
-            for(int left=1; left<=n-window+1; left++){
-			
-                int right = left+window-1;
-                for(int i=left; i<=right; i++){
-                    dp[left][right] = Math.max(dp[left][right], 
-                    (arr[left-1] * arr[i] * arr[right+1]) + dp[left][i-1] + dp[i+1][right]);
-                }
+    public int maxCoins(int[] arr) {
+        int N = arr.length;
+        int[][] dp = new int[N+2][N+2];
+        int[] nums = new int[N+2];
+
+        nums[0] = 1;
+        nums[N+1] = 1;
+
+        for(int i = 1; i <=N; i++) nums[i] = arr[i-1];
+        for(int gap = 1; gap <=N; gap++){
+
+            for(int left = 1; left <= N - gap + 1; left++){
+                int right = left + gap - 1;
+                for(int k = left; k <= right; k++)
+            dp[left][right] = Math.max(dp[left][right], nums[left-1] * nums[k] * nums[right+1] + dp[left][k-1] + dp[k+1][right]);
+
             }
         }
-        return dp[1][n];
+
+        return dp[1][N];
     }
 }
