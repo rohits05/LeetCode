499fcd3df82a83265aade6e95662177654d3a78c
Rohit Shaw <carpediem212045@gmail.com>
2023-05-05 13:18:14 +0530

    Time: 1 ms (71.49%), Space: 44.5 MB (14.42%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,24 @@
+class Solution { 
+    public void setZeroes(int[][] mat) {
+        int n =mat.length, m =mat[0].length;
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> col = new HashSet<>();
+        
+        for(int i=0; i<n; i++){ // Finding specific R & C with 0's!
+           for(int j=0; j<m; j++){ // O(N2)
+               if(mat[i][j] == 0){
+                    row.add(i); 
+                    col.add(j);
+               }
+           }
+        } 
+        
+        for(int r: row){
+            for(int j=0; j<m; j++) mat[r][j] = 0; // Setting that row as 0!
+        }
+        
+        for(int c: col){
+            for(int i=0; i<n; i++) mat[i][c] = 0; // Setting that col as 0!
+        }
+    }
+}
