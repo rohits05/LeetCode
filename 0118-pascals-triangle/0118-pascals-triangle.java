835a380e2c8932e3968a9788e4eac21f72a1c913
Rohit Shaw <carpediem212045@gmail.com>
2023-05-05 13:32:22 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (9.61%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,18 +1,31 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-	    List<Integer>row,prev = null;
+    public List<List<Integer>> generate(int n) {
+//      List<List<Integer>> result = new ArrayList<List<Integer>>();
+// 	    List<Integer>row,prev = null;
         
-	    for(int i=0; i<numRows; i++){
-	    	row = new ArrayList<Integer>();
+// 	    for(int i=0; i<numRows; i++){
+// 	    	row = new ArrayList<Integer>();
             
-	    	for(int j=0; j<=i; ++j)
-	    		if(j == 0 || j == i) row.add(1);
-                else row.add(prev.get(j - 1) + prev.get(j));     
-            prev = row;
-            result.add(row);
-	    }
+// 	    	for(int j=0; j<=i; ++j)
+// 	    		if(j == 0 || j == i) row.add(1);
+//                 else row.add(prev.get(j - 1) + prev.get(j));     
+//             prev = row;
+//             result.add(row);
+// 	    }
         
-	    return result;
+// 	    return result;
+        
+        int[][] arr = new int[n][];
+        
+        for(int i=0; i<n; i++){
+            int[] row = new int[i + 1];
+            row[0] = 1; row[i] = 1;
+            for(int j=1; j<i; j++)
+                row[j] = arr[i - 1][j - 1] + arr[i - 1][j];
+            arr[i] = row;
+        }
+        
+        return (List)Arrays.asList(arr);
     }
 }
+
