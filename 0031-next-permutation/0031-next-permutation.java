811a54109f3a8760863ec95ed7e9a72d87f5a39e
Rohit Shaw <carpediem212045@gmail.com>
2023-05-06 12:46:26 +0530

    Time: 0 ms (100.00%), Space: 43 MB (22.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,17 +1,15 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         if(nums == null || nums.length <= 1) return;
-        // Break Point
+
         int i = nums.length - 2;
         while(i >= 0 && nums[i] >= nums[i + 1]) i--;
         
-        // If we get the break points
         if(i >= 0){
             int j = nums.length - 1;
-            while( nums[j] <= nums[i]) j--;
+            while(nums[j] <= nums[i]) j--;
             swap(nums , i , j);
         }
-        //If no break point exists , Simply reverse the right half
         reverse(nums, i + 1, nums.length - 1);
     }
     
@@ -24,5 +22,4 @@ class Solution {
     public void reverse(int[] nums , int i , int j){
         while(i < j) swap(nums , i++ ,j--);
     }
-  
 }
