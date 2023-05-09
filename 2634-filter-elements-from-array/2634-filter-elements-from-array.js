9c45426a244aa2cd89fb53e4c2316187bbf74d26
Rohit Shaw <carpediem212045@gmail.com>
2023-05-09 11:52:35 +0530

    Time: 60 ms (44.72%), Space: 41.8 MB (80.96%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let temp =[];
+    let j =0, n =arr.length;
+    for(let i=0; i<n; i++){
+        if(fn(arr[i], i)) temp[j++] =  arr[i];
+    }
+    
+    return temp;
+};
