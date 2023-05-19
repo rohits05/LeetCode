36bc7c516217f7ddb3cf464840086dcde7177050
Rohit Shaw <carpediem212045@gmail.com>
2023-05-19 12:14:49 +0530

    Time: 0 ms (100.00%), Space: 56.3 MB (98.10%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         // Approach 1
-//         int temp[] = nums.clone();
-//         k = k % nums.length;   
-//         for(int i=0; i<nums.length; i++){
-//             nums[(i+k) % nums.length] = temp[i];
-//         }
+//      int temp[] = nums.clone();
+//      k = k % nums.length;   
+//      for(int i=0; i<nums.length; i++){
+//          nums[(i+k) % nums.length] = temp[i];
+//      }
             
         // OpTimizeD Approach
-        int n = nums.length;
-        k = k % n;
+        int n =nums.length;
+        k = k % n; // bound
         reverse(nums, 0, n-1); 
         reverse(nums, 0, k-1);
         reverse(nums, k, n-1); 
@@ -24,6 +24,3 @@ class Solution {
         }
     }
 }
-
-    
-   
