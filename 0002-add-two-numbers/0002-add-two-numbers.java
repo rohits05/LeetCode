7e4a592355856c60f51d4ff964a8cba5881d324c
Rohit Shaw <carpediem212045@gmail.com>
2023-06-16 21:36:40 +0530

    Time: 2 ms (99.23%), Space: 43.8 MB (9.49%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode head = dummyHead;
+        int carry = 0;
+        
+        while(l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val; // tnc
+            int val2 = l2 == null ? 0 : l2.val;
+            
+            int sum = val1 + val2 + carry;
+            head.next = new ListNode(sum % 10); // opt
+            carry = sum / 10;
+            head = head.next;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        
+        if(carry > 0) head.next = new ListNode(carry); // fwd carry
+        return dummyHead.next;
+    }
+}
