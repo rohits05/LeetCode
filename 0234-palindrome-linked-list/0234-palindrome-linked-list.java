bee831ae78655973cca03f8a68a85f138b5fe237
Rohit Shaw <carpediem212045@gmail.com>
2023-06-19 21:52:12 +0530

    Time: 4 ms (85.36%), Space: 56.2 MB (82.29%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head){
+        if(head == null || head.next == null) return true;
+        ListNode slow = head, fast = head;
+        
+        // Finding Mid Elm
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        slow.next = reverseList(slow.next); // Reversing right half
+        slow = slow.next; // Moving slow to right half
+        
+        while(slow != null){
+            if(head.val != slow.val) return false;
+            head = head.next;
+            slow = slow.next;
+        }
+        
+        // To Re-MoDiFY The LL 
+          // ListNode dummy;
+//         ListNode slow = dummy;
+//         ListNode fast = dummy;
+        
+//         // Finding Mid Elm
+//         while(fast.next != null && fast.next.next != null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+        
+//         slow.next = reverseList(slow.next); // Reversing right half
+        return true;
+    }
+    
+    private ListNode reverseList(ListNode head){
+        ListNode prev = null, next = null;
+        
+        while(head !=  null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
