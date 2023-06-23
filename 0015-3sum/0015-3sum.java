1565a7f8e5e601d5464984baab928cb7b70ac797
Rohit Shaw <carpediem212045@gmail.com>
2023-06-23 16:12:40 +0530

    Time: 39 ms (84.39%), Space: 51.7 MB (15.52%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] num) {
+       int n =num.length;
        Arrays.sort(num);
        List<List<Integer>> res = new LinkedList<>();
         
-       for(int i=0 ;i<num.length-2 ;i++){ //(0-Third last el) for Triplet
+       for(int i=0; i<n-2; i++){ // (0-Third last el) for Triplet
            
-           if(i==0 || (i>0 && num[i] != num[i-1])){
-               int low = i+1,high =num.length-1;
-               int sum = 0 - num[i];
+           if(i == 0 || (i>0 && num[i] != num[i-1])){
+               int low = i+1,high =n-1, sum = 0-num[i];
                
                while(low < high){
-                   
-                   if(num[low] + num[high] ==sum){
+                   if(num[low] + num[high] == sum){
                        res.add(Arrays.asList(num[i], num[low], num[high]));
                        
-                       while(low < high && num[low]==num[low+1]) // Ignoring the Duplicates
+                       while(low < high && num[low] == num[low+1]) // Ignoring the Duplicates
                            low++;
-                       while(low < high && num[high]==num[high-1])
+                       while(low < high && num[high] == num[high-1])
                            high--;
 
                        low++;
