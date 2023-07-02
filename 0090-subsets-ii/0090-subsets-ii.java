5d9a881923929d1ea7a6bf526baaa8d275769d19
Rohit Shaw <carpediem212045@gmail.com>
2023-07-02 10:44:07 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (8.73%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] arr) {
+        Arrays.sort(arr);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        func(0, arr, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    static void func(int ind, int nums[], List<Integer> d, 
+                     List<List<Integer>> ans){
+         ans.add(new ArrayList<>(d)); 
+        
+         for(int i=ind; i<nums.length; i++){
+             if(i != ind && nums[i] == nums[i-1]) continue;
+             d.add(nums[i]);
+             func(i+1, nums, d, ans);
+             d.remove(d.size() - 1);
+         }
+     }
+}
