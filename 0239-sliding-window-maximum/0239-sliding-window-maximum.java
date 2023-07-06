5433f6030194af66d468b803b2041f7467c5977b
Rohit Shaw <carpediem212045@gmail.com>
2023-07-06 22:18:28 +0530

    Time: 39 ms (61.18%), Space: 61.2 MB (37.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -6,8 +6,8 @@ class Solution {
         Deque<Integer> q = new ArrayDeque<>();
 
         for(int i=0; i<n; i++){
-            while (!q.isEmpty() && q.peekFirst() == i-k) q.pollFirst();
-            while (!q.isEmpty() && nums[q.peekLast()] <= nums[i]) q.pollLast();
+            while(!q.isEmpty() && q.peekFirst() == i-k) q.pollFirst();
+            while(!q.isEmpty() && nums[q.peekLast()] <= nums[i]) q.pollLast();
 
             q.offerLast(i);
             if(i >= k-1) ans[index++] = nums[q.peekFirst()];
