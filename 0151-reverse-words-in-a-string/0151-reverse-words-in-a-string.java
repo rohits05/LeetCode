71ef6efdb05fc3d67370b7f92be00c5c58a6871a
Rohit Shaw <carpediem212045@gmail.com>
2023-07-07 10:04:47 +0530

    Time: 6 ms (80.44%), Space: 43.7 MB (42.92%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,29 +1,30 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> st = new Stack<>();
-        String temp ="", ans ="";int n =s.length();
+//         Stack<String> st = new Stack<>();
+//         String temp ="", ans ="";
+//         int n =s.length();
         
-        for(int i=0; i<n; i++){
-            if(s.charAt(i) == ' '){
-                if(temp.length() > 0) st.push(temp);
-                temp = "";
-            }else temp += s.charAt(i);
-        }
-        ans += temp;
+//         for(int i=0; i<n; i++){
+//             if(s.charAt(i) == ' '){
+//                 if(temp.length() > 0) st.push(temp);
+//                 temp = "";
+//             }else temp += s.charAt(i);
+//         }
+//         ans += temp;
         
-        while(!st.isEmpty()) ans = ans + " " + st.pop();
-        if(ans.length() != 0 && ans.charAt(0) == ' ') ans = ans.substring(1);
-        return ans;
+//         while(!st.isEmpty()) ans = ans + " " + st.pop();
+//         if(ans.length() != 0 && ans.charAt(0) == ' ') ans = ans.substring(1);
+//         return ans;
         
         // Using String-Builder!
-//         String[] words = s.split(" ");
-//         StringBuilder dev = new StringBuilder();
-//         for(int i=words.length-1; i>=0; i--) {
-//             if(words[i] != "") {
-//                 dev.append(words[i].trim() + " ");
-//             }
-//         }
+        String[] words = s.split(" ");
+        StringBuilder dev = new StringBuilder();
+        
+        int n = words.length;
+        for(int i=n-1; i>=0; i--) {
+            if(words[i] != "") dev.append(words[i].trim() + " ");
+        }
 
-//         return dev.toString().trim();
+        return dev.toString().trim();
     }
 }
