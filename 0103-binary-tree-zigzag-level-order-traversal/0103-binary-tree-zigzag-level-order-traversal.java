1e506f96372d40b8e2d49616166cbcc9bcfdfe26
Rohit Shaw <carpediem212045@gmail.com>
2023-07-12 16:14:06 +0530

    Time: 1 ms (89.39%), Space: 41.5 MB (56.91%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -19,7 +19,6 @@ class Solution {
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
         
         if(root == null) return wrapList;
-        
         queue.offer(root);
         boolean flag = true; 
         
@@ -28,14 +27,10 @@ class Solution {
             List<Integer> subList = new ArrayList<Integer>(levelNum);
             for(int i=0; i<levelNum; i++){
                 int index = i;
-                if(queue.peek().left != null) 
-                    queue.offer(queue.peek().left);
-                if(queue.peek().right != null) 
-                    queue.offer(queue.peek().right);
-                if(flag == true) 
-                    subList.add(queue.poll().val);
-                else 
-                    subList.add(0, queue.poll().val);
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                if(flag == true) subList.add(queue.poll().val);
+                else subList.add(0, queue.poll().val);
             }  
             flag = !flag; 
             wrapList.add(subList);
