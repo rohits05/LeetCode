b1d29a1a68768afbac6b3a526ddd2a6449275a95
Rohit Shaw <carpediem212045@gmail.com>
2023-09-01 23:03:51 +0530

    Time: 2 ms (56.99%), Space: 45 MB (64.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,11 +4,18 @@ class Solution {
     
         for(int i=0; i<n; i++){
             if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[nonZ];
-                nums[nonZ] = temp;
+                // int temp = nums[i];
+                // nums[i] = nums[nonZ];
+                // nums[nonZ] = temp;
+                swap(nums, i, nonZ);
                 nonZ++;
             }
         }
+    }
+    
+    private void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
