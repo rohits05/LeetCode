2bd98173e5f0e159b0eddf751a1ca3c1d0d0ce0d
Rohit Shaw <carpediem212045@gmail.com>
2023-09-08 11:14:44 +0530

    Time: 3 ms (35.34%), Space: 41.4 MB (36.11%) - LeetHub

---
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumOperations(String num) {
+        int n = num.length();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('0', '0');
+        map.put('2', '5');
+        map.put('5', '0');
+        map.put('7', '5');
+        
+        HashSet<Character> set = new HashSet<>();
+        boolean containsZero = false;
+        
+        for(int i=n-1; i>=0; i--){
+            char ch = num.charAt(i);
+            if(ch == '0') containsZero = true;
+            if(map.containsKey(ch) && set.contains(map.get(ch))) return n-i-2;
+            set.add(ch);
+        }
+        
+        return ((containsZero) ? (n-1) : n);
+    }
+}
