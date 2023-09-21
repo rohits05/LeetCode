c97c2da339f513c3450adfc2d2486e11d271b1d6
Rohit Shaw <carpediem212045@gmail.com>
2023-09-21 09:55:58 +0530

    Time: 4 ms (100.00%), Space: 44.6 MB (16.74%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -4,8 +4,7 @@ class Solution {
 
     public List<String> findItinerary(List<List<String>> tickets) { 
         for(List<String> ticket: tickets){ 
-            String start = ticket.get(0); 
-            String end = ticket.get(1);
+            String start = ticket.get(0), end = ticket.get(1);
             PriorityQueue<String> destinations = map.getOrDefault(start, new PriorityQueue<>());
             destinations.offer(end);
             map.put(start, destinations);
