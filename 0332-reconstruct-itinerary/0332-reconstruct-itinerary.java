e38743c96d88beb1bb6082340a310795d2e96ee6
Rohit Shaw <carpediem212045@gmail.com>
2023-09-21 09:54:58 +0530

    Time: 4 ms (100.00%), Space: 44.1 MB (76.63%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -0,0 +1,27 @@
+class Solution {
+    private HashMap<String, PriorityQueue<String>> map = new HashMap<>(); 
+    private LinkedList<String> res = new LinkedList<>();
+
+    public List<String> findItinerary(List<List<String>> tickets) { 
+        for(List<String> ticket: tickets){ 
+            String start = ticket.get(0); String end = ticket.get(1);
+            PriorityQueue<String> destinations = map.getOrDefault(start, new PriorityQueue<>());
+            destinations.offer(end);
+            map.put(start, destinations);
+        }
+
+        dfs("JFK");
+        return res;
+    }
+
+    private void dfs(String src) { 
+        PriorityQueue<String> destinations = map.get(src);
+        while(destinations!=null && destinations.size()>0){
+            String next = destinations.poll();
+            dfs(next);
+        }
+        
+        res.addFirst(src);
+        return;
+    }
+}
