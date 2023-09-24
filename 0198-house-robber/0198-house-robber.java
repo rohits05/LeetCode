81d29e95a4d9a77298d0c1b5d6b2af019c82b209
Rohit Shaw <carpediem212045@gmail.com>
2023-09-24 09:30:29 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (21.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,7 +4,6 @@ class Solution {
         if(n == 1) return arr[0];
         
         int a =0, b = arr[0];
-        
         for(int i=2; i<=n; i++){
             int c = Math.max(b, a+arr[i-1]);
             a = b; b = c;
