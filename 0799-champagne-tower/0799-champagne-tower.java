db08d2701e54ff8551bcf25ff213c4582f3a0d4d
Rohit Shaw <carpediem212045@gmail.com>
2023-10-10 18:01:21 +0530

    Time: 2 ms (98.25%), Space: 39.1 MB (99.99%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -4,7 +4,8 @@ class Solution {
         curLvl[0] = poured;
         
         for(int row=1; row<=query_row; row++){
-            for(int i=row; i>=0; i--) curLvl[i+1] += curLvl[i] = Math.max(0.0, (curLvl[i]-1) / 2);
+            for(int i=row; i>=0; i--) 
+                curLvl[i+1] += curLvl[i] = Math.max(0.0, (curLvl[i]-1) / 2);
         }
         
         return Math.min(curLvl[query_glass], 1.0);
