70a4633045c81a5a20ae52cba401ebaedbd916f1
Rohit Shaw <carpediem212045@gmail.com>
2023-10-28 15:46:59 +0530

    Time: 944 ms (80.70%), Space: 0B (100.00%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,6 +1,3 @@
 # Write your MySQL query statement below
-select product_id from Products
-where low_fats like 'Y' and recyclable like 'Y';
--- WHERE low_fats IN('Y') AND recyclable IN('Y') 
--- WHERE low_fats LIKE 'Y%' AND recyclable LIKE 'Y%'
--- WHERE low_fats REGEXP '^Y' AND recyclable REGEXP '^Y'
+SELECT product_id FROM Products
+WHERE low_fats LIKE "Y" AND recyclable LIKE "Y";
