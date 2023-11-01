15d0583f85f7cb36e1d3fd91db7123731a7807b0
Rohit Shaw <carpediem212045@gmail.com>
2023-11-01 12:16:49 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (56.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -15,7 +15,7 @@ class Solution {
         }
     }
     
-    public void swap(int a[], int i, int j){
+    private void swap(int a[], int i, int j) {
         int t = a[i]; a[i] = a[j]; a[j] = t;
     }
 }
