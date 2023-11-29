c8a7681ee952ba21061aeee2af00cbd9efd4fba9
Rohit Shaw <carpediem212045@gmail.com>
2023-11-29 11:06:19 +0530

    Time: 1 ms (99.30%), Space: 43.4 MB (21.56%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -25,27 +25,23 @@ class Pair {
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
-        
         int ans = 0;
         Queue<Pair> q = new LinkedList<>();
         q.offer(new Pair(root , 0));
         
         while(!q.isEmpty()){
-            int size = q.size();
-            int mmin = q.peek().num; // Start from 0 - Id
-            int first = 0,last = 0;
+            int size = q.size(), mmin = q.peek().num; 
+            int first = 0, last = 0;
             
-            for(int i=0 ; i<size ; i++){
+            for(int i=0; i<size; i++){
                 int cur_id = q.peek().num-mmin;
                 TreeNode node = q.peek().node;
                 q.poll();
                 
                 if(i == 0) first = cur_id;
                 if(i == size-1) last = cur_id;
-                if(node.left != null)
-                    q.offer(new Pair(node.left, cur_id*2+1)); // left Cal - Node
-                if(node.right != null)
-                    q.offer(new Pair(node.right, cur_id*2+2)); // right Cal - Node
+                if(node.left != null) q.offer(new Pair(node.left, cur_id*2+1)); // left Cal - Node
+                if(node.right != null) q.offer(new Pair(node.right, cur_id*2+2)); // right Cal - Node
             }
             ans = Math.max(ans, last-first+1); // Width of every level
         }
