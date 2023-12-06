6477d6313075d39e8e8956d9f5e84bba10cc57ce
Rohit Shaw <carpediem212045@gmail.com>
2023-12-06 17:09:44 +0530

    Time: 6 ms (96.67%), Space: 55.3 MB (51.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,47 +1,31 @@
 class Solution {
-    Map<String,Boolean> palindromes = new HashMap<String,Boolean>();
-	Map<Integer,List<Integer>> adj_map = new HashMap<Integer,List<Integer>>();
-	List<List<String>> results;
-	
-	public List<List<String>> partition(String s) {
-		results = new LinkedList<List<String>>();
-		if(s == null || s.length() == 0) return results;	
-		for(int i=0; i<s.length(); i++) adj_map.put(i, new LinkedList<Integer>());
-		
-		for(int i=1; i<=s.length(); i++){
-			for(int j=i-1; j>=0; j--){
-				if(isPalindromes(s.substring(j, i))) adj_map.get(j).add(i);
-			}
-		}
-		
-		resultBuilder(0, s.length(), s, new LinkedList<String>());
-		return results;
-	}
-	
-	private void resultBuilder(int start,int end,String s,List<String> result){
-		if(start == end){
-			results.add(new ArrayList<String>(result));
-			return;
-		}
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> p = new ArrayList<>();
+        func(0, s, p, ans);
+        return ans;
+    }
+    
+    void func(int idx, String s, List<String> p, List<List<String>> res){
+        if(idx == s.length()){
+            res.add(new ArrayList<>(p));
+            return;
+        }
         
-		for(Integer i: adj_map.get(start)){
-			result.add(s.substring(start, i));
-			resultBuilder(i, end, s, result);
-			result.remove(result.size()-1);
-		}
-	}
-	
-	public boolean isPalindromes(String s){
-		if(palindromes.containsKey(s)) return palindromes.get(s);
-		
-		for(int i=0, j=s.length()-1; j>i; i++, j--){
-			if(s.charAt(i) != s.charAt(j)){
-				palindromes.put(s, false);
-				return false;
-			}
-		}
+        for(int i=idx; i<s.length(); i++){
+            if(isPal(s, idx, i)){
+                p.add(s.substring(idx, i+1));
+                func(i+1, s, p, res);
+                p.remove(p.size() - 1);
+            }
+        }
+    }
+    
+    boolean isPal(String s, int start, int end){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)) return false;
+        }
         
-		palindromes.put(s, true);
-		return true;
-	}
+        return true;
+    }
 }
