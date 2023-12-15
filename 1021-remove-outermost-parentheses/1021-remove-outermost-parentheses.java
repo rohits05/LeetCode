1d0d3981c2e16010306d7c34c7834aa1fac285f3
Rohit Shaw <carpediem212045@gmail.com>
2023-12-15 19:32:19 +0530

    Time: 9 ms (25.58%), Space: 41.4 MB (50.12%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -3,13 +3,17 @@ class Solution {
         StringBuilder s = new StringBuilder();
         Stack<Character> h = new Stack<>();
 
-        for (int i=0; i<S.length(); i++){
-            if (h.size() == 1 && S.charAt(i) == ')') h.pop();
-            else if (h.size() == 0 && S.charAt(i) == '(') h.push('(');
-            else if (S.charAt(i) == ')')
-            { s.append(")"); h.pop(); }
-            else if (S.charAt(i) == '(')
-            { s.append("("); h.push('('); }
+        for(int i=0; i<S.length(); i++){
+            if(h.size() == 1 && S.charAt(i) == ')') h.pop();
+            else if(h.size() == 0 && S.charAt(i) == '(') h.push('(');
+            else if(S.charAt(i) == ')'){ 
+                s.append(")"); 
+                h.pop(); 
+            }
+            else if(S.charAt(i) == '('){ 
+                s.append("("); 
+                h.push('('); 
+            }
         }
         
         return s.toString();
